c315f19f69b2dc9d52ad87e023fac6e3f26e4eb1
Vance Morrison <vancem@microsoft.com>
2015-06-29 14:06:32 -0700

    Update version number, release notes

--- src/PerfView/Properties/AssemblyInfo.cs
+++ src/PerfView/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.7.24.0")]
+[assembly: AssemblyFileVersion("1.7.25.0")]
 [assembly:  InternalsVisibleTo("PerfViewTests")]
