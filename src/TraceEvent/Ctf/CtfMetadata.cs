71f37552965d8178af43f71cd303d06a747ea07f
Lee Culver <leculver@microsoft.com>
2015-11-09 13:08:56 -0800

    Implement CTF metadata parser
    Build a simple CTF metadata parser.

---
+++ src/TraceEvent/Ctf/CtfMetadata.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Microsoft.Diagnostics.Tracing.Ctf
+{
+    /// <summary>
+    /// The parsed metadata.
+    /// </summary>
+    class CtfMetadata
+    {
+        private CtfTrace _trace;
+        private CtfEnvironment _env;
+        private Dictionary<string, CtfClock> _clocks = new Dictionary<string, CtfClock>();
+        private List<CtfStream> _streams = new List<CtfStream>();
+        private List<CtfEvent> _events = new List<CtfEvent>();
+        private Dictionary<string, CtfMetadataType> _typeAlias = new Dictionary<string, CtfMetadataType>();
+        
+
+        public CtfTrace Trace { get { return _trace; } }
+        public CtfEnvironment Environment { get { return _env; } }
+        public IList<CtfStream> Streams { get { return _streams; } }
+        public IList<CtfEvent> Events { get { return _events; } }
+        public ICollection<CtfClock> Clocks { get { return _clocks.Values; } }
+
+
+        public CtfMetadata()
+        {
+        }
+
+        public CtfMetadata(CtfMetadataParser parser)
+        {
+            Load(parser);
+        }
+
+
+        public void Load(CtfMetadataParser parser)
+        {
+            foreach (CtfMetadataDeclaration entry in parser.Parse())
+            {
+                switch (entry.Definition)
+                {
+                    case CtfDeclarationTypes.Clock:
+                        CtfClock clock = new CtfClock(entry.Properties);
+                        _clocks[clock.Name] = clock;
+                        break;
+
+                    case CtfDeclarationTypes.Trace:
+                        _trace = new CtfTrace(entry.Properties);
+                        break;
+
+                    case CtfDeclarationTypes.Environment:
+                        _env = new CtfEnvironment(entry.Properties);
+                        break;
+
+                    case CtfDeclarationTypes.TypeAlias:
+                        _typeAlias[entry.Name] = entry.Type;
+                        break;
+
+                    case CtfDeclarationTypes.Struct:
+                        _typeAlias[entry.Name] = new CtfStruct(entry.Fields);
+                        break;
+
+                    case CtfDeclarationTypes.Stream:
+                        CtfStream stream = new CtfStream(entry.Properties);
+                        while (_streams.Count <= stream.ID)
+                            _streams.Add(null);
+
+                        _streams[stream.ID] = stream;
+                        break;
+
+                    case CtfDeclarationTypes.Event:
+                        CtfEvent evt = new CtfEvent(entry.Properties);
+                        while (_events.Count <= evt.ID)
+                            _events.Add(null);
+
+                        _events[(int)evt.ID] = evt;
+                        break;
+
+                    default:
+                        Debug.Fail("Unknown metadata entry type.");
+                        break;
+                }
+            }
+
+
+            ResolveReferences();
+        }
+        
+        private void ResolveReferences()
+        {
+            _trace.ResolveReferences(_typeAlias);
+
+            foreach (CtfStream stream in _streams)
+                stream.ResolveReferences(_typeAlias);
+
+            foreach (CtfEvent evt in _events)
+                evt.ResolveReferences(_typeAlias);
+
+        }
+    }
+
+
+
+    class CtfTrace
+    {
+        public short Major { get; private set; }
+        public short Minor { get; private set; }
+        public Guid UUID { get; private set; }
+        public string ByteOrder { get; private set; }
+        public CtfStruct Header { get; private set; }
+
+        public CtfTrace(CtfPropertyBag bag)
+        {
+            Major = bag.GetShort("major");
+            Minor = bag.GetShort("minor");
+            UUID = new Guid(bag.GetString("uuid"));
+            ByteOrder = bag.GetString("byte_order");
+            Header = bag.GetStruct("packet.header");
+        }
+
+        internal void ResolveReferences(Dictionary<string, CtfMetadataType> typealias)
+        {
+            Header = (CtfStruct)Header.ResolveReference(typealias);
+        }
+    }
+
+    class CtfStream
+    {
+        public int ID { get; private set; }
+        public CtfMetadataType EventHeader { get; private set; }
+        public CtfMetadataType PacketContext { get; private set; }
+
+        public CtfStream(CtfPropertyBag properties)
+        {
+            ID = properties.GetInt("id");
+            EventHeader = properties.GetType("event.header");
+            PacketContext = properties.GetType("packet.context");
+        }
+
+        internal void ResolveReferences(Dictionary<string, CtfMetadataType> _typeAlias)
+        {
+            EventHeader = EventHeader.ResolveReference(_typeAlias);
+            PacketContext = PacketContext.ResolveReference(_typeAlias);
+        }
+    }
+
+    class CtfEnvironment
+    {
+        public CtfEnvironment(CtfPropertyBag bag)
+        {
+            HostName = bag.GetString("hostname");
+            Domain = bag.GetString("domain");
+            TracerName = bag.GetString("tracer");
+            TracerMajor = bag.GetIntOrNull("tracer_major") ?? 0;
+            TracerMinor = bag.GetIntOrNull("tracer_minor") ?? 0;
+        }
+
+        public string Domain { get; private set; }
+        public string HostName { get; private set; }
+        public string TracerName { get; private set; }
+        public int TracerMajor { get; private set; }
+        public int TracerMinor { get; private set; }
+    }
+
+    class CtfClock
+    {
+        public CtfClock(CtfPropertyBag bag)
+        {
+            Name = bag.GetString("name");
+            UUID = new Guid(bag.GetString("uuid"));
+            Description = bag.GetString("description");
+            Frequency = bag.GetUlong("freq");
+            Offset = bag.GetUlong("offset");
+        }
+
+        public string Description { get; private set; }
+        public ulong Frequency { get; private set; }
+        public string Name { get; private set; }
+        public ulong Offset { get; private set; }
+        public Guid UUID { get; private set; }
+    }
+
+    class CtfEvent
+    {
+        public int ID { get; private set; }
+        public string Name { get; private set; }
+        public uint Stream { get; private set; }
+        public uint LogLevel { get; private set; }
+        public CtfField[] Fields { get; private set; }
+
+        public CtfEvent(CtfPropertyBag bag)
+        {
+            ID = bag.GetInt("id");
+            Name = bag.GetString("name");
+            Stream = bag.GetUInt("stream_id");
+            LogLevel = bag.GetUInt("loglevel");
+
+            Fields = bag.GetStruct("fields").Fields;
+        }
+
+        internal void ResolveReferences(Dictionary<string, CtfMetadataType> typealias)
+        {
+            foreach (CtfField field in Fields)
+                field.ResolveReference(typealias);
+        }
+    }
+}
---
+++ src/TraceEvent/Ctf/CtfMetadataTypes.cs
@@ -0,0 +1,255 @@
+﻿using System.Collections.Generic;
+
+namespace Microsoft.Diagnostics.Tracing.Ctf
+{
+    enum CtfTypes
+    {
+        Unknown,
+        Unresolved,
+        String,
+        Integer,
+        Struct,
+        Array,
+        Enum,
+        Variant,
+        Float
+    }
+
+    abstract class CtfMetadataType
+    {
+        public CtfTypes CtfType { get; protected set; }
+
+        public CtfMetadataType(CtfTypes type)
+        {
+            CtfType = type;
+        }
+
+        internal abstract CtfMetadataType ResolveReference(Dictionary<string, CtfMetadataType> typealias);
+    }
+
+    class CtfUnresolvedType : CtfMetadataType
+    {
+        public string Name { get; private set; }
+
+        public CtfUnresolvedType(string name)
+            : base(CtfTypes.Unresolved)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        internal override CtfMetadataType ResolveReference(Dictionary<string, CtfMetadataType> typealias)
+        {
+            return typealias[Name];
+        }
+    }
+
+    class CtfArray : CtfMetadataType
+    {
+        public CtfArray(CtfMetadataType type, string index)
+            : base(CtfTypes.Array)
+        {
+            Type = type;
+            Index = index;
+        }
+
+        public CtfMetadataType Type { get; private set; }
+        public string Index { get; private set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0}[{1}]", Type, Index);
+        }
+
+        internal override CtfMetadataType ResolveReference(Dictionary<string, CtfMetadataType> typealias)
+        {
+            Type = Type.ResolveReference(typealias);
+            return this;
+        }
+    }
+
+    class CtfFloat : CtfMetadataType
+    {
+        public CtfFloat(CtfPropertyBag bag)
+            : base(CtfTypes.Float)
+        {
+            Exp = bag.GetInt("exp_dig");
+            Mant = bag.GetInt("mant_dig");
+            ByteOrder = bag.GetString("byte_order");
+            Align = bag.GetShortOrNull("align");
+        }
+
+        public short? Align { get; private set; }
+        public string ByteOrder { get; private set; }
+        public int Exp { get; private set; }
+        public int Mant { get; private set; }
+
+        internal override CtfMetadataType ResolveReference(Dictionary<string, CtfMetadataType> typealias)
+        {
+            return this;
+        }
+    }
+
+    class CtfInteger : CtfMetadataType
+    {
+        public CtfInteger(CtfPropertyBag bag)
+            : base(CtfTypes.Integer)
+        {
+            Size = bag.GetShort("size");
+            Align = bag.GetShortOrNull("align") ?? 8;
+            Signed = bag.GetBoolean("signed");
+            Encoding = bag.GetString("encoding") ?? "none";
+            Base = bag.GetShortOrNull("base") ?? 10;
+            Map = bag.GetString("map");
+        }
+
+        public short Size { get; private set; }
+        public short Align { get; private set; }
+        public bool Signed { get; private set; }
+        public string Encoding { get; private set; }
+        public short Base { get; private set; }
+        public string Map { get; private set; }
+
+        public override string ToString()
+        {
+            return (Signed ? "int" : "uint") + Size.ToString();
+        }
+
+        internal override CtfMetadataType ResolveReference(Dictionary<string, CtfMetadataType> typealias)
+        {
+            return this;
+        }
+    }
+
+    class CtfString : CtfMetadataType
+    {
+        public CtfString()
+            : base(CtfTypes.String)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "string";
+        }
+
+        internal override CtfMetadataType ResolveReference(Dictionary<string, CtfMetadataType> typealias)
+        {
+            return this;
+        }
+    }
+
+    class CtfEnum : CtfMetadataType
+    {
+        public CtfEnum(CtfMetadataType type, CtfNamedRange[] ranges)
+            : base(CtfTypes.Enum)
+        {
+            Type = type;
+            Values = ranges;
+        }
+
+        public CtfMetadataType Type { get; private set; }
+        public CtfNamedRange[] Values { get; private set; }
+
+        internal override CtfMetadataType ResolveReference(Dictionary<string, CtfMetadataType> typealias)
+        {
+            Type = Type.ResolveReference(typealias);
+            return this;
+        }
+    }
+
+    class CtfStruct : CtfMetadataType
+    {
+        bool _resolved = false;
+
+        public CtfField[] Fields { get; private set; }
+
+        public CtfStruct(CtfField[] fields)
+            : base(CtfTypes.Struct)
+        {
+            Fields = fields;
+        }
+
+        internal override CtfMetadataType ResolveReference(Dictionary<string, CtfMetadataType> typealias)
+        {
+            if (!_resolved)
+                foreach (CtfField field in Fields)
+                    field.ResolveReference(typealias);
+
+            _resolved = true;
+            return this;
+        }
+    }
+
+    class CtfVariant : CtfMetadataType
+    {
+        bool _resolved = false;
+        public CtfVariant(string switchName, CtfField[] union)
+            : base(CtfTypes.Variant)
+        {
+            Switch = switchName;
+            Union = union;
+        }
+
+        public string Switch { get; private set; }
+        public CtfField[] Union { get; private set; }
+
+        internal override CtfMetadataType ResolveReference(Dictionary<string, CtfMetadataType> typealias)
+        {
+            if (!_resolved)
+                foreach (CtfField field in Union)
+                    field.ResolveReference(typealias);
+
+            _resolved = true;
+            return this;
+        }
+    }
+
+    class CtfField
+    {
+        public CtfMetadataType Type { get; private set; }
+        public string Name { get; private set; }
+
+        public CtfField(CtfMetadataType type, string name)
+        {
+            Type = type;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        internal void ResolveReference(Dictionary<string, CtfMetadataType> typealias)
+        {
+            Type = Type.ResolveReference(typealias);
+        }
+    }
+
+    struct CtfNamedRange
+    {
+        public string Name;
+        public int Begin;
+        public int End;
+
+        public CtfNamedRange(string name, int begin, int end)
+        {
+            Name = name;
+            Begin = begin;
+            End = end;
+        }
+
+        public override string ToString()
+        {
+            if (Begin == End)
+                return string.Format("{0} = {1}", Name, Begin);
+
+            return string.Format("{0} = {1} ... {2}", Name, Begin, End);
+        }
+    }
+}
