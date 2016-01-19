ae5df18f8b5ad8b01290957040c0e2038fde4c0d
Luca Duran <t-lufern@microsoft.com>
2016-01-19 13:14:55 -0800

    Add utils

---
+++ src/PerfView/LinuxPerfView/Shared/Utils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Validation;
+
+namespace LinuxPerfView.Shared
+{
+	internal static class Utils
+	{
+		internal static long ConcatIntegers(int left, int right)
+		{
+			Requires.Argument(right >= 0, nameof(right), "Must be positive");
+			return int.Parse(string.Format("{0}{1}", left, right));
+		}
+	}
+}
