33e29c46cfc957e4887d137a3e82da485d543bdc
Kaelin Hooper <t-kahoop@microsoft.com>
2016-06-08 17:06:12 -0700

    Moved project into an Electron application. There are some bugs that will have to be worked out (e.g. context menu no longer working, and the navbar is a little wonky)

---
+++ src/HtmlJs/perfview/main.js
@@ -0,0 +1,37 @@
+const electron = require('electron')
+
+const {app} = electron;
+
+const {BrowserWindow} = electron;
+
+
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({width: 800, height: 600});
+
+  win.loadURL('file://' + __dirname + '/index.html');
+
+  win.webContents.openDevTools();
+
+  win.on('closed', () => {
+    win = null;
+  })
+}
+
+
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+})
