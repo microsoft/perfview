1d6382890dcadf87ab0755a679aa590943f41d19
Vance Morrison <vancem@microsoft.com>
2016-06-15 10:11:06 -0700

    Fix issue where opening a .trace.zip file failes with a file in use error when opening CPU stacks.
    The issue was twofold.
    
    First we were opening ZIP files in exclusive mode when reading, that is bad, use ZipFile.Open which does the right thing (allows other simultaneous readers)
    However a related issue is that we did not close the ZIP file when the CTF open failed.   This left the file open so that the next open (to open stacks) would fail.

--- src/TraceEvent/CtfTraceEventSource.cs
+++ src/TraceEvent/CtfTraceEventSource.cs
@@ -45,52 +45,65 @@ namespace Microsoft.Diagnostics.Tracing
         {
             _filename = fileName;
             _zip = ZipFile.Open(fileName, ZipArchiveMode.Read);
-
-            _channels = new List<Tuple<ZipArchiveEntry, CtfMetadata>>();
-            foreach (ZipArchiveEntry metadataArchive in _zip.Entries.Where(p => Path.GetFileName(p.FullName) == "metadata"))
+            bool success = false;
+            try
             {
-                CtfMetadataLegacyParser parser = new CtfMetadataLegacyParser(metadataArchive.Open());
-                CtfMetadata metadata = new CtfMetadata(parser);
 
-                string path = Path.GetDirectoryName(metadataArchive.FullName);
-                _channels.AddRange(from entry in _zip.Entries
-                                   where Path.GetDirectoryName(entry.FullName) == path && Path.GetFileName(entry.FullName).StartsWith("channel")
-                                   select new Tuple<ZipArchiveEntry, CtfMetadata>(entry, metadata));
+                _channels = new List<Tuple<ZipArchiveEntry, CtfMetadata>>();
+                foreach (ZipArchiveEntry metadataArchive in _zip.Entries.Where(p => Path.GetFileName(p.FullName) == "metadata"))
+                {
+                    CtfMetadataLegacyParser parser = new CtfMetadataLegacyParser(metadataArchive.Open());
+                    CtfMetadata metadata = new CtfMetadata(parser);
 
-                pointerSize = Path.GetDirectoryName(metadataArchive.FullName).EndsWith("64-bit") ? 8 : 4;
-            }
+                    string path = Path.GetDirectoryName(metadataArchive.FullName);
+                    _channels.AddRange(from entry in _zip.Entries
+                                       where Path.GetDirectoryName(entry.FullName) == path && Path.GetFileName(entry.FullName).StartsWith("channel")
+                                       select new Tuple<ZipArchiveEntry, CtfMetadata>(entry, metadata));
+
+                    pointerSize = Path.GetDirectoryName(metadataArchive.FullName).EndsWith("64-bit") ? 8 : 4;
+                }
 
 
-            IntPtr mem = Marshal.AllocHGlobal(sizeof(TraceEventNativeMethods.EVENT_RECORD));
-            TraceEventNativeMethods.ZeroMemory(mem, sizeof(TraceEventNativeMethods.EVENT_RECORD));
-            _header = (TraceEventNativeMethods.EVENT_RECORD*)mem;
+                IntPtr mem = Marshal.AllocHGlobal(sizeof(TraceEventNativeMethods.EVENT_RECORD));
+                TraceEventNativeMethods.ZeroMemory(mem, sizeof(TraceEventNativeMethods.EVENT_RECORD));
+                _header = (TraceEventNativeMethods.EVENT_RECORD*)mem;
 
-            int processors = (from entry in _channels
-                              let filename = entry.Item1.FullName
-                              let i = filename.LastIndexOf('_')
-                              let processor = filename.Substring(i + 1)
-                              select int.Parse(processor)
-                             ).Max() + 1;
+                int processors = (from entry in _channels
+                                  let filename = entry.Item1.FullName
+                                  let i = filename.LastIndexOf('_')
+                                  let processor = filename.Substring(i + 1)
+                                  select int.Parse(processor)
+                                 ).Max() + 1;
 
-            numberOfProcessors = processors;
+                numberOfProcessors = processors;
 
-            // TODO: Need to cleanly separate clocks, but in practice there's only the one clock.
-            CtfClock clock = _channels.First().Item2.Clocks.First();
+                // TODO: Need to cleanly separate clocks, but in practice there's only the one clock.
+                CtfClock clock = _channels.First().Item2.Clocks.First();
 
-            long firstEventTimestamp = (long)new ChannelList(_channels).First().Current.Timestamp;
+                var channels = new ChannelList(_channels);
+                if (channels.Count() == 0)
+                    throw new EndOfStreamException("No CTF Information found in ZIP file.");
 
-            _QPCFreq = (long)clock.Frequency;
-            sessionStartTimeQPC = firstEventTimestamp;
-            _syncTimeQPC = firstEventTimestamp;
-            _syncTimeUTC = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).AddSeconds((clock.Offset - 1) / clock.Frequency);
+                long firstEventTimestamp = (long)channels.First().Current.Timestamp;
 
-            _eventMapping = InitEventMap();
+                _QPCFreq = (long)clock.Frequency;
+                sessionStartTimeQPC = firstEventTimestamp;
+                _syncTimeQPC = firstEventTimestamp;
+                _syncTimeUTC = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).AddSeconds((clock.Offset - 1) / clock.Frequency);
 
+                _eventMapping = InitEventMap();
+                success = true;
 #if DEBUG
             //// Uncomment for debug output.
             //_debugOut = File.CreateText("debug.txt");
             //_debugOut.AutoFlush = true;
 #endif
+            }
+            finally
+            {
+                if (!success)
+                    Dispose();      // This closes the ZIP file we opened.  We don't want to leave it dangling.  
+            }
         }
 
         private static Dictionary<string, ETWMapping> InitEventMap()
@@ -577,7 +590,13 @@ namespace Microsoft.Diagnostics.Tracing
         protected override void Dispose(bool disposing)
         {
             if (disposing)
-                _zip.Dispose();
+            {
+                if (_zip != null)
+                {
+                    _zip.Dispose();
+                    _zip = null;
+                }
+            }
 
             // TODO
             //Marshal.FreeHGlobal(new IntPtr(_header));
