e7e10d3468678ab7e771c1433faa33122e1bc1ff
Kaelin Hooper <t-kahoop@microsoft.com>
2016-06-21 19:35:26 -0700

    Stripped out most of PerfView and got some very stripped down version of Extensibility.cs to compile.

--- src/HtmlJs/PerfViewService/Models/CSVReader/ETLStackBrowse/ZL/BlockType.cs
+++
@@ -1,8 +0,0 @@
-namespace System.IO.Compression2 {
-    internal enum BlockType {
-        Uncompressed = 0,
-        Static = 1,
-        Dynamic = 2
-    }
-}
-
--- src/HtmlJs/PerfViewService/Models/EtwClrProfiler/compileManifest.bat
+++
@@ -1,2 +0,0 @@
-REM This generates a ETWClrProfiler.h file
-.\MC.exe -W winmeta.xml -um -b ETWClrProfiler.man 
