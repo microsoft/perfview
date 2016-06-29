ade436f9b97f4f6225d92a079aa36635ccd36588
Kaelin Hooper <t-kahoop@microsoft.com>
2016-06-29 15:00:59 -0700

    Got initial stage of changing directories and opening a .etl.zip file working

--- src/HtmlJs/PerfDataService/Views/js/Delegate.js
+++ src/HtmlJs/PerfDataService/Views/js/Delegate.js
@@ -2,37 +2,99 @@ function Delegate() {
     var self = this;
     self.domain = "http://localhost:5000";
     self.defaultDirectoryTreePath = "C:/Users/t-kahoop/Development/perfview/src/PerfView/bin/Debug";
+    self.treeDivID = "#treeContainer";
 
-  this.log = function log(status) {
-    $("#statusBar span").text(status);
-  }
+    self.log = function log(status) {
+        $("#statusBar span").text(status);
+    };
 
-  this.httpGet = function httpGet(url, callback) {
-    url = self.domain + url;
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() {
-      if (xmlHttp.readyState == 4) {
-        if (xmlHttp.status == 200) {
-            self.log("GET " + url + " Complete");
-            callback(JSON.parse(xmlHttp.responseText));
-        } else {
-          self.log("GET " + url + " " + xmlHttp.status);
-        }
-      }
+    // PRIVATE
+    function httpGet(url, callback) {
+        url = self.domain + url;
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function () {
+            if (xmlHttp.readyState == 4) {
+                if (xmlHttp.status == 200) {
+                    self.log("GET " + url + " Complete");
+                    callback(JSON.parse(xmlHttp.responseText));
+                } else {
+                    self.log("GET " + url + " " + xmlHttp.status);
+                }
+            }
+        };
+        xmlHttp.open("GET", url, true);
+        xmlHttp.send(null);
+    };
+
+    self.changeDirectoryTreePath = function changeDirectoryTreePath(path) {
+        httpGet("/api/data/open?path=" + path, function (response) {
+            console.log(response);
+            createJsTreeFromJSON(response);
+            delegate.log("Completed: Directory Tree Path Update");
+        });
+    };
+
+    // PRIVATE
+    function createJsTreeFromJSON(JSONTree) {
+        // In case there is a tree already loaded in the div, destroy it
+        $(self.treeDivID).jstree("destroy");
+
+        // Load the JSON data
+        $(self.treeDivID).jstree({
+            'core': {
+                'data': JSONTree.children
+            }
+        });
+
+        // Add JsTree event listeners (handles user interactions with the tree)
+        addJsTreeEventListeners();
+    };
+
+    // PRIVATE
+    function addJsTreeEventListeners() {
+        $(self.treeDivID).on('activate_node.jstree', function (event, node) {
+            self.changeDirectoryTreePath(node.node.original.path);  // JSTree has a node within a node.. Weird.
+        });
+
+        // TODO: Use double click event to change directory
+        //$(self.treeDivID).on('dblclick.jstree', function (event, node) {
+        //    var node = $(event.target).closest("li");
+        //    var node_id = node[0].id
+        //    var newPath = $(self.treeDivID).find(node_id);
+
+        //    console.log(newPath);
+        //    self.changeDirectoryTreePath(node.node.original.path);
+        //});
+        
+        // TODO: Get dropdown arrows working
+        //$(self.treeDivID).on('load_node.jstree', function (event, data) {
+        //    var treeNode = data.node;
+        //    if (treeNode.type === NODE_TYPE_FOLDER) {
+        //        domNode = $(self.treeDivID).jstree.get_node(treeNode.id, true);
+        //        domNode.addClass('jstree-open').removeClass('jstree-leaf');
+        //    }
+        //});
     }
-    xmlHttp.open("GET", url, true);
-    xmlHttp.send(null);
-  }
 
-  this.changeDirectoryTreePath = function changeDirectoryTreePath(path) {
-      self.httpGet("/api/data/open?path=" + path, function (response) {
-          console.log(response);
-          var items = response.children;
-          var htmlFromJSON = createTreeFromJSON(items);
-          $("#directoryTree").html(htmlFromJSON);
-          delegate.log("Completed: Directory Tree Path Update");
-      });
-  }
 }
 
 delegate = new Delegate();
+
+
+
+/**********************************************************/
+/**********************************************************/
+/**************** USING THE DELEGATE BELOW ****************/
+/**********************************************************/
+/**********************************************************/
+
+
+
+$(document).ready(function () {
+    delegate.changeDirectoryTreePath(delegate.defaultDirectoryTreePath);
+});
+
+$(document).ready(function () {
+    // TODO: Pull these from a cache file of the 10 most recently options the user has chosen
+    $("#recentDirectories").append("<option value=" + delegate.defaultDirectoryTreePath + "/>");
+});
