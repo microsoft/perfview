eed5378f3447434f1373f8ed9ed9a881414d4f64
Kaelin Hooper <t-kahoop@microsoft.com>
2016-07-12 11:06:09 -0700

    Split Delegate.js into homeDelegate.js and stackDelegate.js

---
+++ src/HtmlJs/PerfDataService/Views/js/homeDelegate.js
@@ -0,0 +1,74 @@
+﻿function HomeDelegate() {
+    self = this;
+    self.domain = "http://localhost:5000";
+    self.defaultDirectoryTreePath = "C:/Users/t-kahoop/Development/perfview/src/PerfView/bin/Debug";
+    self.treeDivID = "#treeContainer";
+
+    self.log = function log(status) {
+        $("#statusBar span").text(status);
+    };
+
+    self.changeDirectoryTreePath = function changeDirectoryTreePath(path) {
+        url = self.domain + "/api/data/open?path=" + path;
+        $.get(url, function (response, status) {
+            json = JSON.parse(response);
+            console.log(json);
+            self.log("GET " + url + " Complete");
+            createJsTreeFromJSON(json);
+        });
+    };
+
+
+    function createJsTreeFromJSON(JSONTree) {
+        // In case there is a tree already loaded in the div, destroy it
+        $(self.treeDivID).jstree("destroy");
+
+        // Load the JSON data
+        $(self.treeDivID).jstree({
+            'core': {
+                'data': JSONTree.children
+            }
+        });
+
+        // Add JsTree event listeners (handles user interactions with the tree)
+        addJsTreeEventListeners();
+
+        self.log("Completed: Directory Tree Path Update");
+    };
+
+
+    function addJsTreeEventListeners() {
+        $(self.treeDivID).on('activate_node.jstree', function (event, node) {
+            nodeObject = node.node.original;  // JSTree has a node within a node.. Weird.
+
+            if (nodeObject.type == "dir" || nodeObject.type == "file") {
+                self.changeDirectoryTreePath(nodeObject.path);
+            } else if (nodeObject.type == "stacks") {
+                self.openStackSummary(nodeObject.path, nodeObject.stackType, self.defaultNumNodes);
+            }
+        });
+    }
+
+    self.openStackSummary = function openStackSummary(filename, stackType, numNodes) {
+        $.get("/api/data/stackviewer/summary?filename=" + filename + "&stacktype=" + stackType + "&numNodes=" + numNodes, function (response, status) {
+            json = JSON.parse(response);
+
+            console.log(json);
+
+            // Attach data to current (parent) window so that the new window can access it on load (via window.opener.stackData)
+            window.filename = filename;
+            window.stackType = stackType;
+            window.numNodes = numNodes;
+            window.stackData = json;
+
+            // Create and open the new window
+            var stackViewerWindow = window.open(self.domain + "/Views/static/stackviewer.html");
+
+            // Log the completed work out
+            self.log("Completed: Open " + stackType + " Stack Summary at filepath " + filename);
+        });
+    }
+
+}
+
+homeDelegate = new HomeDelegate();
