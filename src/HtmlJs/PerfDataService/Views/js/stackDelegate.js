b18a5a7462a2049472ae155bd960c4cfb3e1f66f
Kaelin Hooper <t-kahoop@microsoft.com>
2016-07-26 15:42:20 -0700

    Callee tab is working

--- src/HtmlJs/PerfDataService/Views/js/stackDelegate.js
+++ src/HtmlJs/PerfDataService/Views/js/stackDelegate.js
@@ -8,14 +8,17 @@ function StackDelegate(domain, filename, stackType, defaultNumNodes, summaryStac
     self.focusNode = null;
     self.filters = ""
 
+
     self.log = function log(status) {
         $("#statusBar span").text(status);
     };
 
+
     // TODO: Probably remove this
-    $('#tabs').on('change.zf.tabs', function (event, tab) {
-        //console.log(tab);
-    });
+    //$('#tabs').on('change.zf.tabs', function (event, tab) {
+    //    //console.log(tab);
+    //});
+
 
     self.getSummaryData = function getSummaryData(callback) {
         var url = self.domain + "/api/data/stackviewer/summary?filename=" + self.filename
@@ -23,7 +26,6 @@ function StackDelegate(domain, filename, stackType, defaultNumNodes, summaryStac
                                                           + "&numNodes=" + self.defaultNumNodes
                                                           + "&" + self.filters;
 
-        //console.log("Fetching Summary Data for " + self.filename);
         self.log("Fetching Summary Data for " + self.filename);
 
         $.get(url, function (response, status) {
@@ -31,11 +33,11 @@ function StackDelegate(domain, filename, stackType, defaultNumNodes, summaryStac
             self.summaryStackData = json;
 
             self.log("Completed: Get Summary Data for " + self.filename);
-            //console.log("Completed: Get Summary Data for " + self.filename);
 
             callback(json);
         });
     }
+
 
     self.getCallersData = function getCallersData(nodeName, path, callback) {
         var url = self.domain + "/api/data/stackviewer/callertree?filename=" + self.filename
@@ -46,15 +48,34 @@ function StackDelegate(domain, filename, stackType, defaultNumNodes, summaryStac
                                                                        + "&" + self.filters;
 
         path = path != "" && path != undefined ? "/" + path : path;
-        //console.log("Fetching Callers Data for " + nodeName + path);
         self.log("Fetching Callers Data for " + nodeName + path);
 
         $.get(url, function (response, status) {
             json = JSON.parse(response);
 
             self.log("Completed: Get Callers for " + nodeName + path);
-            //console.log("Completed: Get Callers for " + nodeName + path);
             
+            callback(json, status);
+        });
+    }
+
+
+    self.getCalleesData = function getCalleesData(nodeName, path, callback) {
+        var url = self.domain + "/api/data/stackviewer/calleetree?filename=" + self.filename
+                                                                  + "&name=" + nodeName
+                                                             + "&stacktype=" + self.stackType
+                                                              + "&numNodes=" + self.defaultNumNodes
+                                                                  + "&path=" + path
+                                                                       + "&" + self.filters;
+
+        path = path != "" && path != undefined ? "/" + path : path;
+        self.log("Fetching Callees Data for " + nodeName + path);
+
+        $.get(url, function (response, status) {
+            json = JSON.parse(response);
+
+            self.log("Completed: Get Callees for " + nodeName + path);
+
             callback(json, status);
         });
     }
