acf5e6962ee5c2552e8338503d5222e6545beef0
Vance Morrison <vancem@microsoft.com>
2016-08-03 10:04:51 -0700

    First cut at making TraceEvent build on .NET Core.   Only modified existing code in minor, compatible ways.

--- src/TraceEvent/ETWReloggerTraceEventSource.cs
+++ src/TraceEvent/ETWReloggerTraceEventSource.cs
@@ -4,7 +4,6 @@ using Microsoft.Diagnostics.Tracing.Parsers.Kernel;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Diagnostics.Eventing;
 using System.Diagnostics.Tracing;
 using System.Runtime.InteropServices;
 using TraceReloggerLib;
