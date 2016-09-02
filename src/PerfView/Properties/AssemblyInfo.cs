c5a19de04d8c121ff49e1128ae02e8189b811ca9
Vance Morrison <vancem@microsoft.com>
2016-09-02 15:05:51 -0700

    Added ready-to-run version number to PEFile APIs

--- src/PerfView/Properties/AssemblyInfo.cs
+++ src/PerfView/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information 
 // associated with an assembly.
 [assembly: AssemblyTitle("PerfView")]
