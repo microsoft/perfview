48bf49bd9fa2a35788ff81f41aeba30efc3a1511
Vance Morrison <vancemorrison@comcast.net>
2016-09-16 14:05:09 -0700

    Update Nuget packages to contain the .NET Framework license.

--- src/NugetSupportFiles/PerfView.SupportFiles.MakeNuget.bat
+++ src/NugetSupportFiles/PerfView.SupportFiles.MakeNuget.bat
@@ -1,4 +1,4 @@
-@if NOT EXIST "PerfView.SupportFiles\lib\native\x86\sd.exe"
+@if NOT EXIST "PerfView.SupportFiles\lib\native\x86\sd.exe" (
    echo Error the package does not seem to be populated with binaries.  
    echo Run PerfView.SupportFiles.Populate and update before running. 
    exit /b 1
