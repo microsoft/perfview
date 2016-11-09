87aed956034ba6653274897007b02245e70b33c2
Vance Morrison <vancemorrison@comcast.net>
2016-11-09 12:53:52 -0800

    Update Version to 1.9.42

--- src/PerfView/Properties/AssemblyInfo.cs
+++ src/PerfView/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.9.41.0")]  
+[assembly: AssemblyFileVersion("1.9.42.0")]  
 [assembly:  InternalsVisibleTo("PerfViewTests")] 
