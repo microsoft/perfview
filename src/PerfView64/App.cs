a385f63991ec17a642929540d2209db8429a170c
Sam Harwell <sam.harwell@microsoft.com>
2017-05-07 20:33:18 -0500

    Add a 64-bit launcher for PerfView
    Fixes #175

---
+++ src/PerfView64/App.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace PerfView64
+{
+    public class App
+    {
+        [STAThread]
+        [DebuggerNonUserCode]
+        public static int Main(string[] args)
+        {
+            return PerfView.App.Main(args);
+        }
+    }
+}
