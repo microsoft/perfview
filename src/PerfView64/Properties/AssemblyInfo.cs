11f6a6f6f120c5f42a61874dcdeafbd9f086d4fc
Sam Harwell <sam.harwell@microsoft.com>
2017-05-11 06:52:21 -0500

    Updated PerfView64 assembly info to stay at 1.0.0.0

--- src/PerfView64/Properties/AssemblyInfo.cs
+++ src/PerfView64/Properties/AssemblyInfo.cs
@@ -49,4 +49,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.9.53")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
