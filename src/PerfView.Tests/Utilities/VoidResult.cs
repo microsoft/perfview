f4de3ebba2a9d012916724949f4408b9cd779aa2
Sam Harwell <sam.harwell@microsoft.com>
2017-05-22 00:48:05 -0500

    Implement a regression test for Set Time Range with ru-RU culture
    See #235

---
+++ src/PerfView.Tests/Utilities/VoidResult.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace PerfViewTests.Utilities
+{
+    /// <summary>
+    /// This type provides the type argument for a <see cref="TaskCompletionSource{TResult}"/> in cases where the
+    /// resulting task is intended to represent a <see cref="Task"/> which is not a <see cref="Task{TResult}"/> (i.e.
+    /// one which does not have a result.
+    /// </summary>
+    internal struct VoidResult
+    {
+    }
+}
