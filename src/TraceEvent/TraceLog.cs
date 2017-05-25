683cded82aa8b816f6177678f2c17b4f6c920468
vancem <vancem@VANCEM3>
2017-05-25 07:11:25 -0700

    Review feedback.   (No semantic change)

--- src/TraceEvent/TraceLog.cs
+++ src/TraceEvent/TraceLog.cs
@@ -731,6 +731,9 @@ namespace Microsoft.Diagnostics.Tracing.Etlx
             // (we copy it out below).   To date there are only three parsers that do this (registered, dynamic 
             // (which includes registered), an kernel)   
             // TODO add an option that allows users to add their own here.   
+            // Note that I am not using the variables below, I am fetching the value so that it has the side
+            // effect of creating this parser (which will in turn indicate to the system that I care about the
+            // state these parsers generate as part of their operation).  
             var dynamicParser = source.Dynamic;
             var clrParser = source.Clr;
             var kernelParser = source.Kernel;
