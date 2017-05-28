80fe24619573161491697a68223aa97970483de0
Sam Harwell <sam.harwell@microsoft.com>
2017-05-28 15:23:34 -0500

    Extract a base class PerfViewTestBase for UI tests

---
+++ src/PerfView.Tests/Utilities/PerfViewTestBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.Threading;
+using PerfView;
+
+namespace PerfViewTests.Utilities
+{
+    public abstract class PerfViewTestBase : IDisposable
+    {
+        protected PerfViewTestBase()
+        {
+            // Create the main application
+            AppLog.s_IsUnderTest = true;
+            App.CommandLineArgs = new CommandLineArgs();
+            App.CommandProcessor = new CommandProcessor();
+            GuiApp.MainWindow = new MainWindow();
+
+            JoinableTaskFactory = new JoinableTaskFactory(new JoinableTaskContext());
+        }
+
+        protected JoinableTaskFactory JoinableTaskFactory
+        {
+            get;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                GuiApp.MainWindow?.Close();
+            }
+        }
+    }
+}
