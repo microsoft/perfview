bca0c23e25d161a3df28afbaf84325bbbd32b20e
Sam Harwell <sam.harwell@microsoft.com>
2017-05-28 16:34:50 -0500

    Disable test parallelization for PerfView tests

--- src/PerfView.Tests/Properties/AssemblyInfo.cs
+++ src/PerfView.Tests/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using Xunit;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -31,3 +32,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
