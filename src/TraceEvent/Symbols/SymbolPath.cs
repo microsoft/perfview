2d17ac8c43a9d16475927c7ed3d88406f868cd67
Sam Harwell <sam.harwell@microsoft.com>
2017-06-05 09:11:14 -0500

    Remove unnecessary static state due to caching from ComputerNameExists

--- src/TraceEvent/Symbols/SymbolPath.cs
+++ src/TraceEvent/Symbols/SymbolPath.cs
@@ -5,7 +5,6 @@
 // It is available from http://www.codeplex.com/hyperAddin 
 // #define DEBUG_SERIALIZE
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.ComponentModel; // For Win32Excption;
 using System.Diagnostics;
@@ -258,35 +257,22 @@ namespace Microsoft.Diagnostics.Symbols
         /// </summary>
         public static bool ComputerNameExists(string computerName, int timeoutMSec = 700)
         {
-            bool result;
-            if (s_computerNameLookupCache.TryGetValue(computerName, out result))
-                return result;
+            try
+            {
+                System.Net.IPHostEntry ipEntry = null;
+                var result = System.Net.Dns.BeginGetHostEntry(computerName, null, null);
+                if (result.AsyncWaitHandle.WaitOne(timeoutMSec))
+                    ipEntry = System.Net.Dns.EndGetHostEntry(result);
 
-            return s_computerNameLookupCache.GetOrAdd(
-                computerName,
-                name =>
-                {
-                    try
-                    {
-                        System.Net.IPHostEntry ipEntry = null;
-                        var asyncResult = System.Net.Dns.BeginGetHostEntry(computerName, null, null);
-                        if (asyncResult.AsyncWaitHandle.WaitOne(timeoutMSec))
-                            ipEntry = System.Net.Dns.EndGetHostEntry(asyncResult);
-
-                        if (ipEntry != null)
-                            return true;
-                    }
-                    catch (Exception)
-                    {
-                    }
-                    return false;
-                });
+                if (ipEntry != null)
+                    return true;
+            }
+            catch (Exception) { }
+            return false;
         }
 
         #region private
         private List<SymbolPathElement> m_elements;
-        private static readonly ConcurrentDictionary<string, bool> s_computerNameLookupCache
-            = new ConcurrentDictionary<string, bool>();
 
         /// <summary>
         /// This is the backing field for the lazily-computed <see cref="MicrosoftSymbolServerPath"/> property.
