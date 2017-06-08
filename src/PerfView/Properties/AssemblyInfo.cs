5216f10347e947adc635ebf7e1f7773bbf7b6324
Vance Morrison <vancemorrison@comcast.net>
2017-06-08 08:49:55 -0700

    Update PerfView version number

--- src/PerfView/Properties/AssemblyInfo.cs
+++ src/PerfView/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.9.55")]  
+[assembly: AssemblyFileVersion("1.9.58")]  
 [assembly:  InternalsVisibleTo("PerfViewTests")] 
