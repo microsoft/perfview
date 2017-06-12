25ba6ca2dbe7d411f98212e9cbf0d3733b05c4e8
Sam Harwell <sam.harwell@microsoft.com>
2017-06-12 09:52:42 -0500

    Remove unnecessary method WaitUntilFileIsReady

--- src/LinuxEvent.Tests/Constants.cs
+++ src/LinuxEvent.Tests/Constants.cs
@@ -27,27 +27,5 @@ namespace LinuxTracing.Tests
 		{
 			return Path.Combine(Environment.CurrentDirectory, OutputFolder, string.Format("{0}", filename));
 		}
-
-		public static void WaitUntilFileIsReady(string fileName)
-		{
-            // This used to have logic that polled until the file existed.
-            // This seems like a hack, and the polling logic happened to
-            // open the file read-write, which caused file sharing conflicts
-            // with concurrent tests.    
-
-            // Note that on windows even doing a File.Exists(XXX) LOCKS the
-            // file for a short time.   THus we don't want to do things like
-            // that since it can interfer (this is unfortunate).  If we need
-            // a file probe, we need to do it with a FileOpen with appropriate 
-            // File sharing attributes.  
-
-            // None of this should be necessary.   I am reomving it and if
-            // This method should go away if we don't have problems with it 
-            // for a while. 
-            // -- vance 5/17/2017
-
-            // Thread.Sleep(2);
-            
-		}
 	}
 }
