7bcd88a2be8105a6495605a5a37590cdce798a51
Adam Sitnik <adam.sitnik@gmail.com>
2017-10-23 21:33:31 +0200

    more universal solution for Setting Time Range

--- src/PerfView/StackViewer/StackWindow.xaml.cs
+++ src/PerfView/StackViewer/StackWindow.xaml.cs
@@ -1586,22 +1586,13 @@ namespace PerfView
             var cells = SelectedCells();
             if (cells != null)
             {
-                if (cells.Count == 2) // First and Last column cells
-                {
-                    StartTextBox.Text = GetCellStringValue(cells[0]);
-                    EndTextBox.Text = GetCellStringValue(cells[1]);
-                    Update();
-                }
-                else if (cells.Count == 1) // single cell from a row
-                {
-                    var callTreeNode = cells[0].Item as CallTreeNodeBase;
+                var callTreeNodes = cells.Select(cell => cell.Item).OfType<CallTreeNodeBase>();
 
-                    if (callTreeNode != null)
-                    {
-                        StartTextBox.Text = callTreeNode.FirstTimeRelativeMSec.ToString("n3");
-                        EndTextBox.Text = callTreeNode.LastTimeRelativeMSec.ToString("n3");
-                        Update();
-                    }
+                if (callTreeNodes.Any())
+                {
+                    StartTextBox.Text = callTreeNodes.Min(node => node.FirstTimeRelativeMSec).ToString("n3");
+                    EndTextBox.Text = callTreeNodes.Max(node => node.LastTimeRelativeMSec).ToString("n3");
+                    Update();
                 }
                 else
                     StatusBar.LogError("Could not set time range.");
