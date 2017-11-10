5cec3ba57d18fc95f8edfb04d8ea3eba60860c62
Andrew Wang <waan@microsoft.com>
2017-11-10 09:50:18 -0800

    Adding utils file

---
+++ src/PerfView/StackViewer/StackViewerUtils.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PerfView.StackViewer
+{
+    class StackViewerUtils
+    {
+        public static void PopulateViewMenuItemForPerfDataGrid(PerfDataGrid grid, MenuItem viewMenu)
+        {
+            foreach (DataGridColumn col in grid.Grid.Columns)
+            {
+                // Create MenuItem based off of column header name, make it checkable.
+                // Initially checked because Visibility is set to visable.
+                MenuItem mItem = new MenuItem()
+                {
+                    Header = ((TextBlock)col.Header).Text,
+                    IsCheckable = true,
+                    IsChecked = true
+                };
+
+                // Create Click handler to collapse if unchecked, and make it visable when checked.
+                mItem.Click += delegate (object sender, RoutedEventArgs e)
+                {
+                    MenuItem source = sender as MenuItem;
+                    if (source.IsChecked)
+                    {
+                        col.Visibility = Visibility.Visible;
+                    }
+                    else
+                    {
+                        col.Visibility = Visibility.Collapsed;
+                    }
+                };
+
+                viewMenu.Items.Add(mItem);
+            }
+        }
+    }
+}
