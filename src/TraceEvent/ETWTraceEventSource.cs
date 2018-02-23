6daad15ec2a925f038fbe877730ef176e13bc925
Vance Morrison <vancemorrison@comcast.net>
2018-02-22 17:15:46 -0800

    Fixes issue #478 (partially)
    Basically an old API is broken.  Use a new one when we can.

--- src/TraceEvent/ETWTraceEventSource.cs
+++ src/TraceEvent/ETWTraceEventSource.cs
@@ -458,7 +458,8 @@ namespace Microsoft.Diagnostics.Tracing
             Debug.Assert(_QPCFreq != 0);
             if (pointerSize == 0 || IsRealTime)  // We get on x64 OS 4 as pointer size which is wrong for realtime sessions. Fix it up. 
             {
-                pointerSize = (RuntimeInformation.OSArchitecture == Architecture.X64 || RuntimeInformation.OSArchitecture == Architecture.Arm64) ? 8 : 4;
+                pointerSize = GetOSPointerSize();
+
                 Debug.Assert((logFiles[0].LogFileMode & TraceEventNativeMethods.EVENT_TRACE_REAL_TIME_MODE) != 0);
             }
             Debug.Assert(pointerSize == 4 || pointerSize == 8);
@@ -513,6 +514,23 @@ namespace Microsoft.Diagnostics.Tracing
             };
         }
 
+        /// <summary>
+        /// Returns the size of pointer (8 or 4) for the operating system (not necessarily the process) 
+        /// </summary>
+        internal static int GetOSPointerSize()
+        {
+            if (IntPtr.Size == 8)
+                return 8;
+#if !NETSTANDARD1_6
+            bool is64bitOS = Environment.Is64BitOperatingSystem;
+#else
+            // Sadly this API does not work properly on V4.7.1 of the Desktop framework.   See https://github.com/Microsoft/perfview/issues/478 for more.  
+            // However with this partial fix, (works on everything not NetSTandard, and only in 32 bit processes), that we can wait for the fix.
+            bool is64bitOS = (RuntimeInformation.OSArchitecture == Architecture.X64 || RuntimeInformation.OSArchitecture == Architecture.Arm64);
+#endif
+            return is64bitOS ? 8 : 4;
+        }
+
         internal static DateTime SafeFromFileTimeUtc(long fileTime)
         {
             ulong maxTime = (ulong)DateTime.MaxValue.ToFileTimeUtc();
@@ -533,7 +551,7 @@ namespace Microsoft.Diagnostics.Tracing
                 return qpcTime._GetUTCTimeAsQPC(utcTime);
             }
 
-            #region private
+#region private
             long _GetUTCTimeAsQPC(DateTime utcTime)
             {
                 // Convert to seconds from the baseline
@@ -568,7 +586,7 @@ namespace Microsoft.Diagnostics.Tracing
             DateTime m_timeAsDateTimeUTC;
             long m_timeAsQPC;
 
-            #endregion
+#endregion
         }
 
         internal static List<string> GetMergeAllLogFiles(string fileName)
@@ -792,7 +810,7 @@ namespace Microsoft.Diagnostics.Tracing
                 ret = "";
             return ret;
         }
-        #endregion
+#endregion
     }
 
     /// <summary>
