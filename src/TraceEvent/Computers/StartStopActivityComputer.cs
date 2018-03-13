0f5c9cfa10eca174fcff1d7c7a23084960ad3aae
John Doe <github.john.doe@outlook.com>
2018-03-13 16:17:39 -0700

    Adance -> Advance

--- src/TraceEvent/Computers/StartStopActivityComputer.cs
+++ src/TraceEvent/Computers/StartStopActivityComputer.cs
@@ -709,7 +709,7 @@ namespace Microsoft.Diagnostics.Tracing
                 uint value = 0;
                 if (!secondNibble)
                     value = (uint)(*bytePtr & 0xF);
-                bytePtr++;       // Adance to the value bytes
+                bytePtr++;       // Advance to the value bytes
 
                 numBytes++;     // Now numBytes is 1-4 and represents the number of bytes to read.  
                 if (endPtr < bytePtr + numBytes)
