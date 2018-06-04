c5365282dec1737450d750dd04a5ec0cc7144063
Vance Morrison <vancemorrison@comcast.net>
2018-06-04 15:28:01 -0700

    Added support for Goto Source for NetPerf files
    Most of the logic to support Goto Source 'just works' for NetPerf files but there was an early-out check that the file was an ETL file.    Fix this.
    
    Added a small improvement to what we point in PerfView's log when capturing a GC heap dump.
    
    Added a small improvement to avoid manipulating a kernel session when KernelEvents == None.
    
    Added 'the 'Type' field when showing ClrThreadSamples in the 'Any Stacks' view.

--- src/PerfView/CommandProcessor.cs
+++ src/PerfView/CommandProcessor.cs
@@ -1021,28 +1021,31 @@ namespace PerfView
             // Try to stop the kernel session
             try
             {
-                using (var kernelSession = new TraceEventSession(KernelTraceEventParser.KernelSessionName, TraceEventSessionOptions.Attach))
+               if (parsedArgs.KernelEvents != KernelTraceEventParser.Keywords.None)
                 {
-                    if (parsedArgs.InMemoryCircularBuffer)
+                    using (var kernelSession = new TraceEventSession(KernelTraceEventParser.KernelSessionName, TraceEventSessionOptions.Attach))
                     {
-                        kernelSession.SetFileName(Path.ChangeExtension(parsedArgs.DataFile, ".kernel.etl")); // Flush the file 
-
-                        // We need to manually do a kernel rundown to get the list of running processes and images loaded into memory
-                        // Ideally this is done by the SetFileName API so we can avoid merging.  
-                        var rundownFile = Path.ChangeExtension(parsedArgs.DataFile, ".kernelRundown.etl");
-
-                        // Note that enabling providers is async, and thus there is a concern that we would lose events if we don't wait 
-                        // until the events are logged before shutting down the session.   However we only need the DCEnd events and
-                        // those are PART of kernel session stop, which is synchronous (the session will not die until it is complete)
-                        // so we don't have to wait after enabling the kernel session.    It is somewhat unfortunate that we have both
-                        // the DCStart and the DCStop events, but there does not seem to be a way of asking for just one set.  
-                        using (var kernelRundownSession = new TraceEventSession(s_UserModeSessionName + "Rundown", rundownFile))
+                        if (parsedArgs.InMemoryCircularBuffer)
                         {
-                            kernelRundownSession.BufferSizeMB = 256;    // Try to avoid lost events.  
-                            kernelRundownSession.EnableKernelProvider(KernelTraceEventParser.Keywords.Process | KernelTraceEventParser.Keywords.ImageLoad);
+                            kernelSession.SetFileName(Path.ChangeExtension(parsedArgs.DataFile, ".kernel.etl")); // Flush the file 
+
+                            // We need to manually do a kernel rundown to get the list of running processes and images loaded into memory
+                            // Ideally this is done by the SetFileName API so we can avoid merging.  
+                            var rundownFile = Path.ChangeExtension(parsedArgs.DataFile, ".kernelRundown.etl");
+
+                            // Note that enabling providers is async, and thus there is a concern that we would lose events if we don't wait 
+                            // until the events are logged before shutting down the session.   However we only need the DCEnd events and
+                            // those are PART of kernel session stop, which is synchronous (the session will not die until it is complete)
+                            // so we don't have to wait after enabling the kernel session.    It is somewhat unfortunate that we have both
+                            // the DCStart and the DCStop events, but there does not seem to be a way of asking for just one set.  
+                            using (var kernelRundownSession = new TraceEventSession(s_UserModeSessionName + "Rundown", rundownFile))
+                            {
+                                kernelRundownSession.BufferSizeMB = 256;    // Try to avoid lost events.  
+                                kernelRundownSession.EnableKernelProvider(KernelTraceEventParser.Keywords.Process | KernelTraceEventParser.Keywords.ImageLoad);
+                            }
                         }
+                        kernelSession.Stop();
                     }
-                    kernelSession.Stop();
                 }
             }
             catch (FileNotFoundException) { LogFile.WriteLine("No Kernel events were active for this trace."); }
