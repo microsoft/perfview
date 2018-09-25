36fb3876e38c7eb0fa06d2ee668431cf524307a2
Vance Morrison <vancemorrison@comcast.net>
2018-09-25 13:25:42 -0700

    Typo

--- src/TraceEvent/TraceEventSession.cs
+++ src/TraceEvent/TraceEventSession.cs
@@ -2606,7 +2606,7 @@ namespace Microsoft.Diagnostics.Tracing.Session
         }
 
         /// <summary>
-        /// Returns the GUID of all event provider that either has register itself in a running process (that is
+        /// Returns the GUID of all event provider that either has registered itself in a running process (that is
         /// it CAN be enabled) or that a session has enabled (even if no instances of the provider exist in any process).  
         /// <para>
         /// This is a relatively small list (less than 1000), unlike GetPublishedProviders. 
