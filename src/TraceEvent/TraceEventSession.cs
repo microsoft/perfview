57deb0565159c1116d5116fca3fa214adaa5e95e
Vance Morrison <vancemorrison@comcast.net>
2018-10-31 15:55:15 -0700

    Small fixes, in particular for issue #777
    Fixed issue #777 (Excessive creation of TraceThreads)
    
    Improved logging for finding PDBs slightly (now prints SOMETHING even if you hit a PDB cache).
    
    Fixed a bug where a PDB was not being found because the last lookup had different PDB
    lookup options set.
    
    Responded to issue #766 about the fact that StartTrace does not set the handle to invalid if it fails.
    
    Do a bit better at categorizing threads a .NET ThreadPool threads (or I/O threadpool threads).

--- src/TraceEvent/TraceEventSession.cs
+++ src/TraceEvent/TraceEventSession.cs
@@ -2095,11 +2095,13 @@ namespace Microsoft.Diagnostics.Tracing.Session
             }
             if (retCode == 5 && OperatingSystemVersion.AtLeast(51))     // On Vista and we get a 'Accessed Denied' message
             {
+                m_SessionHandle = TraceEventNativeMethods.INVALID_HANDLE_VALUE; // StartTrace sets to 0 on failure.  We use INVALID_HANDLE_VALUE to represent failure
                 throw new UnauthorizedAccessException("Error Starting ETW:  Access Denied (Administrator rights required to start ETW)");
             }
 
             if (retCode != 0)
             {
+                m_SessionHandle = TraceEventNativeMethods.INVALID_HANDLE_VALUE;  // StartTrace sets to 0 on failure.  We use INVALID_HANDLE_VALUE to represent failure
                 Marshal.ThrowExceptionForHR(TraceEventNativeMethods.GetHRFromWin32(retCode));
             }
 
