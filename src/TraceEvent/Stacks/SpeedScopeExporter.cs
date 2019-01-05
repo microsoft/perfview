f947bf80474fd5f20fb7958374da453113cc6752
Adam Sitnik <adam.sitnik@gmail.com>
2019-01-05 13:30:39 +0100

    SpeedScopeExporter for the purpose of good user story on Linux

---
+++ src/TraceEvent/Stacks/SpeedScopeExporter.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace Microsoft.Diagnostics.Tracing.Stacks
+{
+    public static class SpeedScopeExporter
+    {
+        internal struct Sample
+        {
+            internal StackSourceCallStackIndex StackIndex { get; }
+            internal double RelativeTime { get; }
+            internal double Metric { get; }
+            internal int Depth { get; }
+
+            internal Sample(StackSourceCallStackIndex stackIndex, double relativeTime, double metric, int depth)
+            {
+                StackIndex = stackIndex;
+                RelativeTime = relativeTime;
+                Metric = metric;
+                Depth = depth;
+            }
+
+            public override string ToString() => RelativeTime.ToString(CultureInfo.InvariantCulture);
+        }
+
+        internal enum ProfileEventType : byte
+        {
+            Open = 0, Close = 1 // these values MUST NOT be changed
+        }
+
+        internal struct ProfileEvent
+        {
+            internal ProfileEventType Type { get; }
+            internal int FrameId { get; }
+            internal double RelativeTime { get; }
+            internal int Depth { get; }
+
+            public ProfileEvent(ProfileEventType type, int frameId, double relativeTime, int depth)
+            {
+                Type = type;
+                FrameId = frameId;
+                RelativeTime = relativeTime;
+                Depth = depth;
+            }
+        }
+
+        public static void WriteStackViewAsJson(StackSource source, string filePath)
+        {
+            if (File.Exists(filePath))
+                File.Delete(filePath);
+
+            using (var writeStream = File.CreateText(filePath))
+                Export(source, writeStream, Path.GetFileNameWithoutExtension(filePath));
+        }
+
+        internal static void Export(StackSource source, TextWriter writer, string name)
+        {
+            var sortedSamples = GetSortedSamples(source);
+
+            WalkTheStackAndExpandSamples(source, sortedSamples, out var frameNameToId, out var frameIdToSamples);
+
+            var sortedProfileEvents = GetAggregatedSortedProfileEvents(frameIdToSamples);
+
+            var orderedFrameNames = frameNameToId.OrderBy(pair => pair.Value).Select(pair => pair.Key).ToArray();
+
+            WriteToFile(sortedProfileEvents, orderedFrameNames, writer, name);
+        }
+
+        /// <summary>
+        /// this method gets all samples from StackSource and sorts them by relative time (ascending)
+        /// </summary>
+        internal static IReadOnlyList<Sample> GetSortedSamples(StackSource stackSource)
+        {
+            var samples = new List<Sample>(stackSource.CallStackIndexLimit);
+            stackSource.ForEach(sample => samples.Add(new Sample(sample.StackIndex, sample.TimeRelativeMSec, sample.Metric, -1)));
+            samples.Sort((x, y) => x.RelativeTime.CompareTo(y.RelativeTime));
+
+            return samples;
+        }
+
+        /// <summary>
+        /// all the samples that we have are leafs (last sample in the call stack)
+        /// this method expands those samples to full information 
+        /// it walks the stack up to the begining and adds a sample for every method on the stack
+        /// it's required to build full information
+        /// </summary>
+        internal static void WalkTheStackAndExpandSamples(StackSource stackSource, IEnumerable<Sample> leafs, 
+            out IReadOnlyDictionary<string, int> frameNamesToIds, out IReadOnlyDictionary<int, List<Sample>> frameIdsToSamples)
+        {
+            var frameNameToId = new Dictionary<string, int>();
+            var frameIdToSamples = new Dictionary<int, List<Sample>>();
+
+            var stackOfFrames = new Stack<StackSourceCallStackIndex>();
+
+            foreach (var leafSample in leafs)
+            {
+                // walk the stack first
+                var stackIndex = leafSample.StackIndex;
+                while (stackIndex != StackSourceCallStackIndex.Invalid)
+                {
+                    stackOfFrames.Push(stackIndex);
+
+                    stackIndex = stackSource.GetCallerIndex(stackIndex);
+                }
+
+                // add sample for every method on the stack
+                int depth = -1;
+                while (stackOfFrames.Count > 0)
+                {
+                    stackIndex = stackOfFrames.Pop();
+                    depth++;
+
+                    var frameIndex = stackSource.GetFrameIndex(stackIndex);
+                    if (frameIndex == StackSourceFrameIndex.Broken || frameIndex == StackSourceFrameIndex.Invalid)
+                        continue;
+
+                    var frameName = stackSource.GetFrameName(frameIndex, false);
+                    if (string.IsNullOrEmpty(frameName))
+                        continue;
+
+                    if (!frameNameToId.TryGetValue(frameName, out int exportedFrameId))
+                        frameNameToId.Add(frameName, exportedFrameId = frameNameToId.Count);
+
+                    if (!frameIdToSamples.TryGetValue(exportedFrameId, out var samples))
+                        frameIdToSamples.Add(exportedFrameId, samples = new List<Sample>());
+
+                    // the time and metric are the same as for the leaf sample
+                    // the difference is stack index (not really used from here) and depth (used for sorting the exported data)
+                    samples.Add(new Sample(stackIndex, leafSample.RelativeTime, leafSample.Metric, depth));
+                }
+            }
+
+            frameNamesToIds = frameNameToId;
+            frameIdsToSamples = frameIdToSamples;
+        }
+
+        /// <summary>
+        /// this method aggregates all the singular samples to continuous events
+        /// example: samples for Main taken at time 0.1 0.2 0.3 0.4 0.5
+        /// are gonna be translated to Main start at 0.1 stop at 0.5
+        /// </summary>
+        internal static IReadOnlyList<ProfileEvent> GetAggregatedSortedProfileEvents(IReadOnlyDictionary<int, List<Sample>> frameIdToSamples)
+        {
+            List<ProfileEvent> profileEvents = new List<ProfileEvent>();
+
+            // [0] will be always the Main method, it is our boundry
+            var minRelativeTime = frameIdToSamples[0].First().RelativeTime;
+            var maxRelativeTime = frameIdToSamples[0].Last().RelativeTime;
+
+            foreach (var samplesInfo in frameIdToSamples)
+            {
+                var frameId = samplesInfo.Key;
+                var samples = samplesInfo.Value;
+
+                // this should not be required, but I prefer to be sure that the data is sorted
+                samples.Sort((x, y) => x.RelativeTime.CompareTo(y.RelativeTime));
+
+                Sample openSample = samples[0]; // samples are never empty
+                for (int i = 1; i < samples.Count; i++)
+                {
+                    if (samples[i].RelativeTime > (samples[i - 1].RelativeTime + (samples[i - 1].Metric * 2)) || samples[i].Depth != samples[i - 1].Depth)
+                    {
+                        CenterAndAdd(profileEvents, openSample, samples[i - 1], frameId, minRelativeTime, maxRelativeTime);
+
+                        openSample = samples[i];
+                    }
+                }
+
+                // we need to handle the last (or entire) profile event
+                CenterAndAdd(profileEvents, openSample, samples[samples.Count - 1], frameId, minRelativeTime, maxRelativeTime);
+            }
+
+            // MUST HAVE!!! the tool expects the profile events in certain order!!
+            profileEvents.Sort(CompareProfileEvents);
+
+            return profileEvents;
+        }
+
+        private static void CenterAndAdd(List<ProfileEvent> profileEvents, Sample openSample, Sample closeSample, int frameId, double minRelativeTime, double maxRelativeTime)
+        {
+            // we "center" the samples in order to:
+            // 1: avoid gaps (having method A on stack at 0.1 and method B at 0.2 would create a gap between 0.1 and 0.2 without it
+            // 2. avoid events that Open and Close at the same time for very short method that produced one sample (the tool ignores such events which loses data)
+            double openRelativeTime = openSample.RelativeTime != closeSample.RelativeTime ? openSample.RelativeTime : Math.Max(minRelativeTime, openSample.RelativeTime);
+            double closeRelativeTime = openSample.RelativeTime != closeSample.RelativeTime ? closeSample.RelativeTime :  Math.Min(maxRelativeTime, closeSample.RelativeTime + closeSample.Metric / 2.0);
+            
+            profileEvents.Add(new ProfileEvent(ProfileEventType.Open, frameId, openRelativeTime, openSample.Depth));
+            profileEvents.Add(new ProfileEvent(ProfileEventType.Close, frameId, closeRelativeTime, closeSample.Depth));
+        }
+
+        /// <summary>
+        /// allows for sorting of the profile events in the order that SpeedScope expects them to be written to a file
+        /// </summary>
+        internal static int CompareProfileEvents(ProfileEvent x, ProfileEvent y)
+        {
+            // first of all, we sort ascending by relative time
+            int result = x.RelativeTime.CompareTo(y.RelativeTime);
+            if (result != 0)
+                return result;
+
+            // if both events are open events, then the one with lower depth goes first (it needs to be opened first)
+            if (x.Type == ProfileEventType.Open && y.Type == ProfileEventType.Open)
+                return x.Depth - y.Depth;
+            // if both events are close events, then the one with bigger depth goes first (it needs to be closed first)
+            if (x.Type == ProfileEventType.Close && y.Type == ProfileEventType.Close)
+                return y.Depth - x.Depth;
+
+            // they are of different type so we sort them by type (Open = 0 so it goes first)
+            return x.Type.CompareTo(y.Type);
+        }
+
+        /// <summary>
+        /// writes pre-calculated data to given file in a speedscope-friendly format
+        /// </summary>
+        internal static void WriteToFile(IReadOnlyList<ProfileEvent> sortedProfileEvents, IReadOnlyList<string> orderedFrameNames, TextWriter writer, string name)
+        {
+            writer.Write("{");
+            writer.Write("\"exporter\": \"speedscope@1.3.2\", ");
+            writer.Write($"\"name\": \"{name}\", ");
+            writer.Write("\"activeProfileIndex\": 0, ");
+            writer.Write("\"$schema\": \"https://www.speedscope.app/file-format-schema.json\", ");
+
+            writer.Write("\"shared\": { \"frames\": [ ");
+            for (int i = 0; i < orderedFrameNames.Count; i++)
+            {
+                writer.Write($"{{ \"name\": \"{orderedFrameNames[i].Replace("\\", "\\\\").Replace("\"", "\\\"")}\" }}");
+
+                if (i != orderedFrameNames.Count - 1)
+                    writer.Write(", ");
+            }
+            writer.Write("] }, ");
+
+            writer.Write("\"profiles\": [ {");
+            writer.Write("\"type\": \"evented\", ");
+            writer.Write($"\"name\": \"{name}\", ");
+            writer.Write("\"unit\": \"milliseconds\", ");
+            writer.Write($"\"startValue\": \"{sortedProfileEvents.First().RelativeTime.ToString("R", CultureInfo.InvariantCulture)}\", ");
+            writer.Write($"\"endValue\": \"{sortedProfileEvents.Last().RelativeTime.ToString("R", CultureInfo.InvariantCulture)}\", ");
+            writer.Write("\"events\": [ ");
+            for (int i = 0; i < sortedProfileEvents.Count; i++)
+            {
+                var frameEvent = sortedProfileEvents[i];
+
+                writer.Write($"{{ \"type\": \"{(frameEvent.Type == ProfileEventType.Open ? "O" : "C")}\", ");
+                writer.Write($"\"frame\": {frameEvent.FrameId}, ");
+                // "R" is crucial here!!! we can't loose precision becasue it can affect the sort order!!!!
+                writer.Write($"\"at\": {frameEvent.RelativeTime.ToString("R", CultureInfo.InvariantCulture)} }}");
+
+                if (i != sortedProfileEvents.Count - 1)
+                    writer.Write(", ");
+            }
+            writer.Write("] } ] ");
+
+            writer.Write("}");
+        }
+    }
+}
---
+++ src/TraceEvent/TraceEvent.Tests/SpeedScopeExporterTests.cs
@@ -0,0 +1,256 @@
+﻿using Microsoft.Diagnostics.Tracing.Stacks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace TraceEventTests
+{
+    public class SpeedScopeExporterTests
+    {
+        public class FakeStackSourceSample
+        {
+            public double RelativeTime { get; }
+            public float Metric { get; }
+            public string Name { get; }
+            public StackSourceFrameIndex FrameIndex { get; }
+            public StackSourceCallStackIndex StackIndex { get; }
+            public StackSourceCallStackIndex CallerIndex { get; }
+
+            public FakeStackSourceSample(double relativeTime) => RelativeTime = relativeTime;
+
+            public FakeStackSourceSample(double relativeTime, string name, StackSourceFrameIndex frameIndex,
+                StackSourceCallStackIndex stackIndex, StackSourceCallStackIndex callerIndex)
+            {
+                RelativeTime = relativeTime;
+                Name = name;
+                FrameIndex = frameIndex;
+                StackIndex = stackIndex;
+                CallerIndex = callerIndex;
+            }
+        }
+
+        public class StackSourceStub : StackSource
+        {
+            private readonly IReadOnlyList<FakeStackSourceSample> samples;
+
+            public StackSourceStub(IReadOnlyList<FakeStackSourceSample> fakeStackSourceSamples) => this.samples = fakeStackSourceSamples;
+
+            public override int CallStackIndexLimit => samples.Count;
+
+            public override int CallFrameIndexLimit => samples.Count;
+
+            public override void ForEach(Action<StackSourceSample> callback)
+            {
+                foreach (var stackSourceSample in samples)
+                {
+                    callback(new StackSourceSample(this)
+                    {
+                        TimeRelativeMSec = stackSourceSample.RelativeTime,
+                        Metric = stackSourceSample.Metric,
+                        StackIndex = stackSourceSample.StackIndex
+                    });
+                }
+            }
+
+            public override StackSourceCallStackIndex GetCallerIndex(StackSourceCallStackIndex callStackIndex)
+                => samples.First(sample => sample.StackIndex == callStackIndex).CallerIndex;
+
+            public override StackSourceFrameIndex GetFrameIndex(StackSourceCallStackIndex callStackIndex)
+                => samples.First(sample => sample.StackIndex == callStackIndex).FrameIndex;
+
+            public override string GetFrameName(StackSourceFrameIndex frameIndex, bool verboseName)
+                => samples.First(sample => sample.FrameIndex == frameIndex).Name;
+        }
+
+        [Fact]
+        public void GetSortedSamplesReturnsSamplesSortedByRelativeTime()
+        {
+            var sourceSamples = new[] {
+                new FakeStackSourceSample(0.3),
+                new FakeStackSourceSample(0.1),
+                new FakeStackSourceSample(0.2)
+            };
+
+            var stackSource = new StackSourceStub(sourceSamples);
+
+            var result = SpeedScopeExporter.GetSortedSamples(stackSource);
+
+            Assert.Equal(0.1, result[0].RelativeTime);
+            Assert.Equal(0.2, result[1].RelativeTime);
+            Assert.Equal(0.3, result[2].RelativeTime);
+        }
+
+        [Fact]
+        public void WalkTheStackAndExpandSamplesProducesFullInformation()
+        {
+            // Main() calls A() calls B()
+            const double relativeTime = 0.1;
+            var main = new FakeStackSourceSample(
+                relativeTime: relativeTime,
+                name: "Main",
+                frameIndex: (StackSourceFrameIndex)5, // 5 is first non-taken enum value
+                stackIndex: (StackSourceCallStackIndex)1, // 1 is first non-taken enum value
+                callerIndex: StackSourceCallStackIndex.Invalid);
+            var a = new FakeStackSourceSample(
+                relativeTime: relativeTime,
+                name: "A",
+                frameIndex: (StackSourceFrameIndex)6,
+                stackIndex: (StackSourceCallStackIndex)2,
+                callerIndex: main.StackIndex);
+            var b = new FakeStackSourceSample(
+                relativeTime: relativeTime,
+                name: "B",
+                frameIndex: (StackSourceFrameIndex)7,
+                stackIndex: (StackSourceCallStackIndex)3,
+                callerIndex: a.StackIndex);
+
+            var allSamples = new[] { main, a, b };
+            var leafs = new[] { new SpeedScopeExporter.Sample(b.StackIndex, b.RelativeTime, b.Metric, -1) };
+            var stackSource = new StackSourceStub(allSamples);
+            
+            SpeedScopeExporter.WalkTheStackAndExpandSamples(stackSource, leafs, out var frameNameToId, out var frameIdToSamples);
+
+            Assert.Equal(0, frameNameToId[main.Name]);
+            Assert.Equal(1, frameNameToId[a.Name]);
+            Assert.Equal(2, frameNameToId[b.Name]);
+
+            Assert.All(frameIdToSamples.Select(pair => pair.Value), samples => Assert.Equal(relativeTime, samples.Single().RelativeTime));
+            Assert.Equal(0, frameIdToSamples[0].Single().Depth);
+            Assert.Equal(1, frameIdToSamples[1].Single().Depth);
+            Assert.Equal(2, frameIdToSamples[2].Single().Depth);
+        }
+
+        [Theory]
+        [InlineData(StackSourceFrameIndex.Broken)]
+        [InlineData(StackSourceFrameIndex.Invalid)]
+        public void WalkTheStackAndExpandSamplesHandlesBrokenStacks(StackSourceFrameIndex kind)
+        {
+            // Main() calls WRONG
+            const double relativeTime = 0.1;
+            var main = new FakeStackSourceSample(
+                relativeTime: relativeTime,
+                name: "Main",
+                frameIndex: (StackSourceFrameIndex)5, // 5 is first non-taken enum value
+                stackIndex: (StackSourceCallStackIndex)1, // 1 is first non-taken enum value
+                callerIndex: StackSourceCallStackIndex.Invalid);
+            var wrong = new FakeStackSourceSample(
+                relativeTime: relativeTime,
+                name: "WRONG",
+                frameIndex: kind,
+                stackIndex: (StackSourceCallStackIndex)2,
+                callerIndex: main.StackIndex);
+
+            var allSamples = new[] { main, wrong };
+            var leafs = new[] { new SpeedScopeExporter.Sample(wrong.StackIndex, wrong.RelativeTime, wrong.Metric, -1) };
+            var stackSource = new StackSourceStub(allSamples);
+
+            SpeedScopeExporter.WalkTheStackAndExpandSamples(stackSource, leafs, out var frameNameToId, out var frameIdToSamples);
+
+            Assert.Equal(0, frameNameToId[main.Name]);
+            Assert.False(frameNameToId.ContainsKey(wrong.Name));
+
+            var theOnlySample = frameIdToSamples.Single().Value.Single();
+            Assert.Equal(relativeTime, theOnlySample.RelativeTime);
+            Assert.Equal(0, theOnlySample.Depth);
+        }
+
+        [Fact]
+        public void GetAggregatedSortedProfileEventsAggregates_ContinuousSamplesToOneEvent()
+        {
+            const double metric = 0.1;
+
+            var samples = new[]
+            {
+                new SpeedScopeExporter.Sample((StackSourceCallStackIndex)1, metric: metric, depth: 0, relativeTime: 0.1),
+                new SpeedScopeExporter.Sample((StackSourceCallStackIndex)1, metric: metric, depth: 0, relativeTime: 0.2),
+                new SpeedScopeExporter.Sample((StackSourceCallStackIndex)1, metric: metric, depth: 0, relativeTime: 0.3),
+                new SpeedScopeExporter.Sample((StackSourceCallStackIndex)1, metric: metric, depth: 0, relativeTime: 0.4),
+            };
+
+            var input = new Dictionary<int, List<SpeedScopeExporter.Sample>>() { { 0, samples.ToList() } };
+
+            var aggregatedEvents = SpeedScopeExporter.GetAggregatedSortedProfileEvents(input);
+
+            // we should have an Open at 0.1 (first on the list) and Close at 0.4 (last on the list)
+            Assert.Equal(2, aggregatedEvents.Count);
+
+            Assert.Equal(0.1, aggregatedEvents[0].RelativeTime);
+            Assert.Equal(SpeedScopeExporter.ProfileEventType.Open, aggregatedEvents[0].Type);
+            Assert.Equal(0.4, aggregatedEvents[1].RelativeTime);
+            Assert.Equal(SpeedScopeExporter.ProfileEventType.Close, aggregatedEvents[1].Type);
+        }
+
+        [Fact]
+        public void GetAggregatedSortedProfileEventsAggregates_ContinuousSamplesWithPausesToMultipleEvents()
+        {
+            const double metric = 0.1;
+
+            var samples = new[]
+            {
+                new SpeedScopeExporter.Sample((StackSourceCallStackIndex)1, metric: metric, depth: 0, relativeTime: 0.1),
+                new SpeedScopeExporter.Sample((StackSourceCallStackIndex)1, metric: metric, depth: 0, relativeTime: 0.2),
+
+                new SpeedScopeExporter.Sample((StackSourceCallStackIndex)1, metric: metric, depth: 0, relativeTime: 0.7),
+
+                new SpeedScopeExporter.Sample((StackSourceCallStackIndex)1, metric: metric, depth: 0, relativeTime: 1.1),
+                new SpeedScopeExporter.Sample((StackSourceCallStackIndex)1, metric: metric, depth: 0, relativeTime: 1.2),
+                new SpeedScopeExporter.Sample((StackSourceCallStackIndex)1, metric: metric, depth: 0, relativeTime: 1.3),
+            };
+
+            var input = new Dictionary<int, List<SpeedScopeExporter.Sample>>() { { 0, samples.ToList() } };
+
+            var aggregatedEvents = SpeedScopeExporter.GetAggregatedSortedProfileEvents(input);
+
+            // we should have <0.1, 0.2> and <0.7, 0.75> (the tool would ignore <0.7, 0.7>) and <1.1, 1.3>
+            Assert.Equal(6, aggregatedEvents.Count);
+
+            Assert.Equal(0.1, aggregatedEvents[0].RelativeTime);
+            Assert.Equal(SpeedScopeExporter.ProfileEventType.Open, aggregatedEvents[0].Type);
+            Assert.Equal(0.2, aggregatedEvents[1].RelativeTime);
+            Assert.Equal(SpeedScopeExporter.ProfileEventType.Close, aggregatedEvents[1].Type);
+
+            Assert.Equal(0.7, aggregatedEvents[2].RelativeTime);
+            Assert.Equal(SpeedScopeExporter.ProfileEventType.Open, aggregatedEvents[2].Type);
+            Assert.Equal(0.7 + metric / 2, aggregatedEvents[3].RelativeTime);
+            Assert.Equal(SpeedScopeExporter.ProfileEventType.Close, aggregatedEvents[3].Type);
+
+            Assert.Equal(1.1, aggregatedEvents[4].RelativeTime);
+            Assert.Equal(SpeedScopeExporter.ProfileEventType.Open, aggregatedEvents[4].Type);
+            Assert.Equal(1.3, aggregatedEvents[5].RelativeTime);
+            Assert.Equal(SpeedScopeExporter.ProfileEventType.Close, aggregatedEvents[5].Type);
+        }
+
+        [Fact]
+        public void CompareProfileEventsAllowsForSortingInTheOrderExpectedByTheSpeedScope()
+        {
+            var profileEvents = new List<SpeedScopeExporter.ProfileEvent>()
+            {
+                new SpeedScopeExporter.ProfileEvent(SpeedScopeExporter.ProfileEventType.Open, frameId: 0, depth: 0, relativeTime: 0.1),
+                new SpeedScopeExporter.ProfileEvent(SpeedScopeExporter.ProfileEventType.Open, frameId: 1, depth: 1, relativeTime: 0.1),
+                new SpeedScopeExporter.ProfileEvent(SpeedScopeExporter.ProfileEventType.Close, frameId: 1, depth: 1, relativeTime: 0.3),
+                new SpeedScopeExporter.ProfileEvent(SpeedScopeExporter.ProfileEventType.Close, frameId: 0, depth: 0, relativeTime: 0.3),
+            };
+
+            profileEvents.Reverse(); // reverse to make sure that it does sort the elements in right way
+
+            profileEvents.Sort(SpeedScopeExporter.CompareProfileEvents);
+
+            Assert.Equal(SpeedScopeExporter.ProfileEventType.Open, profileEvents[0].Type);
+            Assert.Equal(0.1, profileEvents[0].RelativeTime);
+            Assert.Equal(0, profileEvents[0].Depth);
+
+            Assert.Equal(SpeedScopeExporter.ProfileEventType.Open, profileEvents[1].Type);
+            Assert.Equal(0.1, profileEvents[1].RelativeTime);
+            Assert.Equal(1, profileEvents[1].Depth);
+
+            Assert.Equal(SpeedScopeExporter.ProfileEventType.Close, profileEvents[2].Type);
+            Assert.Equal(0.3, profileEvents[2].RelativeTime);
+            Assert.Equal(1, profileEvents[2].Depth);
+
+            Assert.Equal(SpeedScopeExporter.ProfileEventType.Close, profileEvents[3].Type);
+            Assert.Equal(0.3, profileEvents[3].RelativeTime);
+            Assert.Equal(0, profileEvents[3].Depth);
+        }
+    }
+}
