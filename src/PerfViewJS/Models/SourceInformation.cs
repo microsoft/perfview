4e36089493b37c149917bfda624c4b2e44e0589e
Mukul Sabharwal <mjsabby@gmail.com>
2019-01-06 11:52:37 -0800

    Add PerfViewJS and delete bitrotted TraceEventAPIServer and HtmlJs

---
+++ src/PerfViewJS/Models/SourceInformation.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace PerfViewJS
+{
+    using System.Collections.Generic;
+    using System.Runtime.Serialization;
+
+    [DataContract]
+    public sealed class SourceInformation
+    {
+        [DataMember]
+        public string Type { get; set; }
+
+        [DataMember]
+        public string Url { get; set; }
+
+        [DataMember]
+        public IEnumerable<LineInformation> Lines { get; set; }
+
+        [DataMember]
+        public IEnumerable<LineInformation> Summary { get; set; }
+
+        [DataMember]
+        public string BuildTimeFilePath { get; set; }
+    }
+}
