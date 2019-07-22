dea111bbc84632cb71328b1dcdb317122f7dd58a
José Rivero <jorive@microsoft.com>
2019-07-22 16:00:59 -0700

    Update the Jit Tail Call Failed event ID This change maps the event to the ID in https://github.com/dotnet/coreclr/blob/2a8529c6130f6c70488e8bace26404b319eed00f/src/vm/ClrEtwAll.man#L3175 The current ID maps the MethodJitInliningFailed event ID, which is wrong.

--- src/TraceEvent/Parsers/ClrTraceEventParser.cs
+++ src/TraceEvent/Parsers/ClrTraceEventParser.cs
@@ -1691,7 +1691,7 @@ namespace Microsoft.Diagnostics.Tracing.Parsers
             add
             {
                 // action, eventid, taskid, taskName, taskGuid, opcode, opcodeName, providerGuid, providerName
-                RegisterTemplate(new MethodJitTailCallFailedTraceData(value, 192, 9, "Method", MethodTaskGuid, 86, "TailCallFailed", ProviderGuid, ProviderName));
+                RegisterTemplate(new MethodJitTailCallFailedTraceData(value, 191, 9, "Method", MethodTaskGuid, 86, "TailCallFailed", ProviderGuid, ProviderName));
             }
             remove
             {
