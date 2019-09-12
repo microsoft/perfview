microsoft/perfview

commit b75c9e90ccfc10acc4452a210ef3efb52c36218c

Increase the history size for events

src/TraceEvent/TraceLog.cs
@@ -4019,7 +4019,7 @@ namespace Microsoft.Diagnostics.Tracing.Etlx
                 public TraceEventCounts CountForEvent;
             }
 
-            private const int historySize = 2048;               // Must be a power of 2
+            private const int historySize = 32768;               // Must be a power of 2
             private PastEventInfoEntry[] pastEventInfo;
             private int curPastEventInfo;                       // points at the first INVALD entry.  
             private TraceLog log;

commit a0e4867c6398b9f617c37f9d7b5810436929dce3

Fix tests
The test failure was first introduced by b75c9e90ccfc10acc4452a210ef3efb52c36218c.

src/PerfView.TestUtilities/ThrowingTraceListener.cs
@@ -21,7 +21,6 @@ namespace PerfView.TestUtilities
     {
         public override void Fail(string message, string detailMessage)
         {
-            Xunit.Assert.True(false, message + Environment.NewLine + detailMessage);
             throw new DebugAssertFailureException(message + Environment.NewLine + detailMessage);
         }
 

src/TraceEvent/TraceLog.cs
@@ -2689,10 +2689,17 @@ namespace Microsoft.Diagnostics.Tracing.Etlx
             public void LogUserStackFragment(CallStackIndex userModeStackIndex, TraceLog eventLog)
             {
                 Debug.Assert(!IsDead);
-                Debug.Assert(UserModeStackIndex == CallStackIndex.Invalid);
-                UserModeStackIndex = userModeStackIndex;
+                if (UserModeStackIndex == CallStackIndex.Invalid)
+                {
+                    UserModeStackIndex = userModeStackIndex;
+                }
+                else
+                {
+                    Debug.Assert(UserModeStackIndex >= 0 && userModeStackIndex < 0);
+                }
+
                 bool emitted = EmitStackForEventIfReady(eventLog);
-                Debug.Assert((emitted && IsDead) || KernelModeStackKey != 0);   // Only not having the kernel stack def can be left
+                Debug.Assert((emitted && IsDead) || KernelModeStackKey != 0 || WaitingToLeaveKernel);   // Only not having the kernel stack def can be left
             }
             public void LogUserStackFragment(Address userModeStackKey, TraceLog eventLog)
             {
@@ -2740,7 +2747,7 @@ namespace Microsoft.Diagnostics.Tracing.Etlx
 
                     // We know that user stacks come after kernel stacks, and we have a user stack.  Thus
                     // if we have a kernel stack or there is no kernel stack, then we can emit it
-                    if (hasKernelStack || KernelModeStackKey == 0)
+                    if ((hasKernelStack || KernelModeStackKey == 0) && !WaitingToLeaveKernel)
                     {
 
                         // If the userModeStack is negative, that means it represents a thread (since we know it can't be