439b31c6a5de0b46736de3d14225147e5de995a9
David Kean <davkean@microsoft.com>
2021-11-14 12:43:31 +1100

    Baseline existing naming warnings
    This suppressions all the violations of the existing naming warnings with the aim to prevent additional warnings and keep clean.

---
+++ src/PerfViewCollect/GlobalSuppressions.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+// Baselined warnings
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Baselined", Scope = "member", Target = "~F:Stats.GcStats.CondemnedReasonsHtmlHeader")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Baselined", Scope = "member", Target = "~F:Stats.ServerGcHistoryEx.activityStats")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Baselined", Scope = "member", Target = "~F:Stats.ServerGcHistoryEx.gcReadyTime")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Baselined", Scope = "member", Target = "~F:Stats.ServerGcHistoryEx.lastGCSpanEndTime")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Baselined", Scope = "member", Target = "~F:Stats.ServerGcHistoryEx.OtherThreadInfo.processName")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Baselined", Scope = "member", Target = "~F:Stats.ServerGcHistoryEx.OtherThreadInfo.runningTime")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Baselined", Scope = "member", Target = "~F:Stats.ServerGcHistoryEx.ServerGCThreadStateInfo.gcThreadRunningTime")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Baselined", Scope = "member", Target = "~F:Stats.ServerGcHistoryEx.ServerGCThreadStateInfo.otherThreadsRunningTime")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Baselined", Scope = "member", Target = "~F:Stats.ServerGcHistoryEx.SGCThreadStateDesc")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Baselined", Scope = "member", Target = "~F:Stats.ServerGcHistoryEx.Type2Color")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Baselined", Scope = "member", Target = "~M:Stats.GcStats.GetCondemnedReasonRow(Microsoft.Diagnostics.Tracing.Analysis.GC.TraceGC,System.Int32@)~System.Byte[]")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Baselined", Scope = "member", Target = "~M:Stats.GcStats.ToHtml(System.IO.TextWriter,Microsoft.Diagnostics.Tracing.Analysis.TraceProcess,Microsoft.Diagnostics.Tracing.Analysis.TraceLoadedDotNetRuntime,System.String,System.Boolean)")]
