819526ab2f0a9a1bf7a4b73e6abd1a08ee7272be
Brian Robbins <brianrob@microsoft.com>
2022-03-25 14:38:35 -0700

    Hide most of the configuration surface, just exposing the currently executing analyzer's configuration.

--- src/TraceEvent/AutomatedAnalysis/AnalyzerExecutionContext.cs
+++ src/TraceEvent/AutomatedAnalysis/AnalyzerExecutionContext.cs
@@ -5,13 +5,34 @@
     /// </summary>
     public sealed class AnalyzerExecutionContext
     {
+        private Configuration _configuration;
+
         internal AnalyzerExecutionContext(Configuration configuration, ITrace trace)
         {
-            Configuration = configuration;
+            _configuration = configuration;
             Trace = trace;
         }
 
-        public Configuration Configuration { get; }
+        /// <summary>
+        /// The configuration for the currently executing Analyzer.
+        /// NULL if no configuration is available.
+        /// </summary>
+        public AnalyzerConfiguration Configuration
+        {
+            get
+            {
+                AnalyzerExecutionScope current = AnalyzerExecutionScope.Current;
+                if (current != null)
+                {
+                    if (_configuration.TryGetAnalyzerConfiguration(current.ExecutingAnalyzer, out AnalyzerConfiguration config))
+                    {
+                        return config;
+                    }
+                }
+
+                return null;
+            }
+        }
 
         /// <summary>
         /// The trace to be analyzed.
