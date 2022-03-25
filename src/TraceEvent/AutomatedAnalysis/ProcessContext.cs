584c3c85ac34bff1b5671e0991ceb536b619e696
Brian Robbins <brianrob@microsoft.com>
2022-03-25 12:34:14 -0700

    Remove unnecessary code from ProcessContext.

--- src/TraceEvent/AutomatedAnalysis/ProcessContext.cs
+++ src/TraceEvent/AutomatedAnalysis/ProcessContext.cs
@@ -8,26 +8,16 @@ namespace Microsoft.Diagnostics.Tracing.AutomatedAnalysis
     public sealed class ProcessContext
     {
         private StackView _cpuStacks;
-        private SymbolReader _symbolReader;
         private AnalyzerExecutionContext _executionContext;
 
         public ProcessContext(AnalyzerExecutionContext executionContext, AnalyzerTraceProcess process)
         {
             _executionContext = executionContext;
             AnalyzerProcess = process;
-            AutomatedAnalysisTraceLog traceLog = executionContext.Trace as AutomatedAnalysisTraceLog;
-            if(traceLog != null)
-            {
-                Process = traceLog.TraceLog.Processes[(ProcessIndex)process.UniqueID];
-            }
-
             Issues = executionContext.Issues[process];
-            _symbolReader = executionContext.SymbolReader;
         }
 
         public AnalyzerTraceProcess AnalyzerProcess { get; }
-
-        public TraceProcess Process { get; }
 
         public List<AnalyzerIssue> Issues { get; }
 
@@ -37,7 +27,7 @@ namespace Microsoft.Diagnostics.Tracing.AutomatedAnalysis
             {
                 if (_cpuStacks == null)
                 {
-                    _cpuStacks = _executionContext.Trace.GetCPUStacks(AnalyzerProcess);
+                    _cpuStacks = _executionContext.Trace.GetStacks(AnalyzerProcess, StackTypes.CPU);
                 }
                 return _cpuStacks;
             }
