77176a3446332bca3ff982ff584767f6c4f5584e
Brian Robbins <brianrob@microsoft.com>
2022-04-20 14:40:09 -0700

    Properly quote JSON keys in DynamicTraceEventParser StructValue.

---
+++ src/TraceEvent/TraceEvent.Tests/Regression/Issue1618.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using Microsoft.Diagnostics.Tracing;
+using Xunit;
+
+namespace TraceEventTests
+{
+    public class Issue1618
+    {
+        [Fact]
+        public void DynamicParserStructSerialization()
+        {
+            string expectedValue = "{ \"b\":\"Hello\", \"c\":\"World!\" }";
+            string inputTraceFile = Path.Combine("inputs", "Regression", "SelfDescribingSingleEvent.etl");
+
+            using(ETWTraceEventSource source = new ETWTraceEventSource(inputTraceFile))
+            {
+                source.Dynamic.AddCallbackForProviderEvent("MySource", "TestEvent", (data) =>
+                {
+                    string jsonStructValue = data.PayloadValue(0).ToString();
+                    Assert.Equal(expectedValue, jsonStructValue);
+                });
+
+                source.Process();
+            }
+        }
+    }
+}
