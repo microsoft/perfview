4bd321569ae5f229e64eaaa1c154a95099e06320
Radek Zikmund <radekzikmund@microsoft.com>
2022-09-20 13:55:40 +0200

    First stab at integrating dark mode from https://github.com/AngryCarrot789/WPFDarkTheme

---
+++ src/PerfView/Themes/ColourfulLightTheme.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+
+namespace REghZyFramework.Themes {
+    public partial class ColourfulLightTheme {
+        private void CloseWindow_Event(object sender, RoutedEventArgs e) {
+            if (e.Source != null)
+                try {
+                    CloseWind(Window.GetWindow((FrameworkElement) e.Source));
+                }
+                catch {
+                }
+        }
+
+        private void AutoMinimize_Event(object sender, RoutedEventArgs e) {
+            if (e.Source != null)
+                try {
+                    MaximizeRestore(Window.GetWindow((FrameworkElement) e.Source));
+                }
+                catch {
+                }
+        }
+
+        private void Minimize_Event(object sender, RoutedEventArgs e) {
+            if (e.Source != null)
+                try {
+                    MinimizeWind(Window.GetWindow((FrameworkElement) e.Source));
+                }
+                catch {
+                }
+        }
+
+        public void CloseWind(Window window) => window.Close();
+
+        public void MaximizeRestore(Window window) {
+            if (window.WindowState == WindowState.Maximized)
+                window.WindowState = WindowState.Normal;
+            else if (window.WindowState == WindowState.Normal)
+                window.WindowState = WindowState.Maximized;
+        }
+
+        public void MinimizeWind(Window window) => window.WindowState = WindowState.Minimized;
+    }
+}
---
+++ src/PerfView/Themes/ThemesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows;
+
+namespace REghZyFramework.Themes {
+    public static class ThemesController {
+        public enum ThemeTypes {
+            Light, ColourfulLight,
+            Dark, ColourfulDark
+        }
+
+        public static ThemeTypes CurrentTheme { get; set; }
+
+        private static ResourceDictionary ThemeDictionary {
+            get { return Application.Current.Resources.MergedDictionaries[0]; }
+            set { Application.Current.Resources.MergedDictionaries[0] = value; }
+        }
+
+        private static void ChangeTheme(Uri uri) {
+            ThemeDictionary = new ResourceDictionary() {Source = uri};
+        }
+
+        public static void SetTheme(ThemeTypes theme) {
+            string themeName = null;
+            CurrentTheme = theme;
+            switch (theme) {
+                case ThemeTypes.Dark:
+                    themeName = "DarkTheme";
+                    break;
+                case ThemeTypes.Light:
+                    themeName = "LightTheme";
+                    break;
+                case ThemeTypes.ColourfulDark:
+                    themeName = "ColourfulDarkTheme";
+                    break;
+                case ThemeTypes.ColourfulLight:
+                    themeName = "ColourfulLightTheme";
+                    break;
+            }
+
+            try {
+                if (!string.IsNullOrEmpty(themeName))
+                    ChangeTheme(new Uri($"Themes/{themeName}.xaml", UriKind.Relative));
+            }
+            catch {
+            }
+        }
+    }
+}
