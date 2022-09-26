3bbafd8f685dc447481e56ad95fd7bb07da3afdf
Radek Zikmund <radekzikmund@microsoft.com>
2022-09-26 12:23:34 +0200

    Add licence notice

--- src/PerfView/Themes/LightTheme.xaml.cs
+++ src/PerfView/Themes/LightTheme.xaml.cs
@@ -1,37 +1,71 @@
-﻿using System.Windows;
+﻿// MIT License
+//
+// Copyright (c) 2021 REghZy
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
-namespace REghZyFramework.Themes {
-    public partial class LightTheme {
-        private void CloseWindow_Event(object sender, RoutedEventArgs e) {
+using System.Windows;
+
+namespace REghZyFramework.Themes
+{
+    public partial class LightTheme
+    {
+        private void CloseWindow_Event(object sender, RoutedEventArgs e)
+        {
             if (e.Source != null)
-                try {
-                    CloseWind(Window.GetWindow((FrameworkElement) e.Source));
+                try
+                {
+                    CloseWind(Window.GetWindow((FrameworkElement)e.Source));
                 }
-                catch {
+                catch
+                {
                 }
         }
 
-        private void AutoMinimize_Event(object sender, RoutedEventArgs e) {
+        private void AutoMinimize_Event(object sender, RoutedEventArgs e)
+        {
             if (e.Source != null)
-                try {
-                    MaximizeRestore(Window.GetWindow((FrameworkElement) e.Source));
+                try
+                {
+                    MaximizeRestore(Window.GetWindow((FrameworkElement)e.Source));
                 }
-                catch {
+                catch
+                {
                 }
         }
 
-        private void Minimize_Event(object sender, RoutedEventArgs e) {
+        private void Minimize_Event(object sender, RoutedEventArgs e)
+        {
             if (e.Source != null)
-                try {
-                    MinimizeWind(Window.GetWindow((FrameworkElement) e.Source));
+                try
+                {
+                    MinimizeWind(Window.GetWindow((FrameworkElement)e.Source));
                 }
-                catch {
+                catch
+                {
                 }
         }
 
         public void CloseWind(Window window) => window.Close();
 
-        public void MaximizeRestore(Window window) {
+        public void MaximizeRestore(Window window)
+        {
             if (window.WindowState == WindowState.Maximized)
                 window.WindowState = WindowState.Normal;
             else if (window.WindowState == WindowState.Normal)
--- src/PerfView/Themes/ThemeViewModel.cs
+++ src/PerfView/Themes/ThemeViewModel.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.ComponentModel;
-using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Input;
 using Utilities;
