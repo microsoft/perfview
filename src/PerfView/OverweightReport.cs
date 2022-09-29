1963cd6337ef170a899069c1b5db8a157e6d61be
marshall-lerner <66791488+marshall-lerner@users.noreply.github.com>
2022-09-29 18:08:09 -0400

    Use the right method (HtmlEncode)

--- src/PerfView/OverweightReport.cs
+++ src/PerfView/OverweightReport.cs
@@ -125,7 +125,7 @@ namespace PerfView
                             continue;
                         }
 
-                        var encodedName = HttpUtility.HttpEncode(r.name);
+                        var encodedName = HttpUtility.HtmlEncode(r.name);
 
                         w.WriteLine("<tr><td>{0}</td><td><a href='command:ShowBaseStacks,{0}' title='View Callers of {0} in Base Stacks'>{1:f1}</a></td><td><a href='command:ShowStacks,{0}' title='View Callers of {0} in Test Stacks'>{2:f1}</a></td><td>{3:f1}</td><td>{4:f2}</td><td>{5:f2}</td><td>{6}</td></tr>",
                             encodedName,
