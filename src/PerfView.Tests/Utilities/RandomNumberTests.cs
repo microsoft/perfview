3b44f2a03414c8e8b1290faa54a7b92021a6a8d3
Brian Robbins <brianrob@microsoft.com>
2023-07-18 16:03:34 -0700

    Implement tests

---
+++ src/PerfView.Tests/Utilities/RandomNumberTests.cs
@@ -0,0 +1,19 @@
+﻿using PerfView.Utilities;
+using Xunit;
+
+namespace PerfViewTests.Utilities
+{
+    public static class RandomNumberTests
+    {
+        [Theory]
+        [InlineData(10000)]
+        public static void GenerateManyDoubles(int numIterations)
+        {
+            for (int i = 0; i < numIterations; i++)
+            {
+                double val = RandomNumberGenerator.GetDouble();
+                Assert.InRange(val, 0.0, 1.0);
+            }
+        }
+    }
+}
