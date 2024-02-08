2233a6a39f2bbc4bba8f73b3cc0dcdf607373f9c
Marshall Lerner <malerner@microsoft.com>
2024-02-08 15:14:33 -0500

    Remove using line

--- src/TraceEvent/Stacks/Stacks.cs
+++ src/TraceEvent/Stacks/Stacks.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation.  All rights reserved
 // This file is best viewed using outline mode (Ctrl-M Ctrl-O)
 
-using Microsoft.Diagnostics.Tracing.AutomatedAnalysis;
 using Microsoft.Diagnostics.Utilities;
 using System;
 using System.Collections.Concurrent;
