15cb4dd4db62e48450794e578596b7a5b5929455
Brian Robbins <brianrob@microsoft.com>
2024-06-11 15:22:46 -0700

    Add symbols authentication samples.

---
+++ src/SymbolsAuth/Samples/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Samples
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            int exampleNum;
+            if (args.Length != 0)
+            {
+                exampleNum = int.Parse(args[0]);
+            }
+            else
+            {
+                PrintUsage();
+                return;
+            }
+
+            switch (exampleNum)
+            {
+                case 1:
+                    SymwebInteractivePopupAuthSample.Run(args);
+                    break;
+                case 2:
+                    SymwebManagedIdentityAuthSample.Run(args);
+                    break;
+                default:
+                    PrintUsage();
+                    break;
+            }
+        }
+
+        private static void PrintUsage()
+        {
+            Console.WriteLine("Samples.exe <ExampleNumber> [optional args for example]");
+            Console.WriteLine("Samples:");
+            Console.WriteLine("\t#1: Symweb interactive pop-up authentication");
+            Console.WriteLine("\tArguments:");
+            Console.WriteLine("\t\tDirectory to store downloaded symbols.");
+            Console.WriteLine("\t\tPath to DLL whose PDB should be looked up on the symbol server.");
+            Console.WriteLine("Example:");
+            Console.WriteLine("\t\"Samples.exe 1\" // Use default values to test symbol server authentication.");
+            Console.WriteLine("\t\"Samples.exe 1 C:\\SymCache Test.dll\" // Specify values to test symbol server authentication.");
+            Console.WriteLine("\t#1: Symweb managed identity authentication");
+            Console.WriteLine("\tArguments:");
+            Console.WriteLine("\t\tDirectory to store downloaded symbols.");
+            Console.WriteLine("\t\tPath to DLL whose PDB should be looked up on the symbol server.");
+            Console.WriteLine("Example:");
+            Console.WriteLine("\t\"Samples.exe 2\" // Use default values to test symbol server authentication.");
+            Console.WriteLine("\t\"Samples.exe 2 C:\\SymCache Test.dll\" // Specify values to test symbol server authentication.");
+        }
+    }
+}
