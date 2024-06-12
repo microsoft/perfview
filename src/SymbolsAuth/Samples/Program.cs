96c6f1e85bcfa7ebf8a2daf6254a9c09614c1a6a
Brian Robbins <brianrob@microsoft.com>
2024-06-11 17:16:23 -0700

    Rename Samples to SymbolsAuth.Samples.

--- src/SymbolsAuth/Samples/Program.cs
+++ src/SymbolsAuth/Samples/Program.cs
@@ -40,16 +40,16 @@ namespace Samples
             Console.WriteLine("\t\tDirectory to store downloaded symbols.");
             Console.WriteLine("\t\tPath to DLL whose PDB should be looked up on the symbol server.");
             Console.WriteLine("Example:");
-            Console.WriteLine("\t\"Samples.exe 1\" // Use default values to test symbol server authentication.");
-            Console.WriteLine("\t\"Samples.exe 1 C:\\SymCache Test.dll\" // Specify values to test symbol server authentication.");
+            Console.WriteLine("\t\"SymbolsAuth.Samples.exe 1\" // Use default values to test symbol server authentication.");
+            Console.WriteLine("\t\"SymbolsAuth.Samples.exe 1 C:\\SymCache Test.dll\" // Specify values to test symbol server authentication.");
             Console.WriteLine("\t#1: Symweb managed identity authentication");
             Console.WriteLine("\tArguments:");
             Console.WriteLine("\t\tClientId of the managed identity.");
             Console.WriteLine("\t\tDirectory to store downloaded symbols.");
             Console.WriteLine("\t\tPath to DLL whose PDB should be looked up on the symbol server.");
             Console.WriteLine("Example:");
-            Console.WriteLine("\t\"Samples.exe 2 <clientid>\" // Use default values to test symbol server authentication.");
-            Console.WriteLine("\t\"Samples.exe 2 <clientid> C:\\SymCache Test.dll\" // Specify values to test symbol server authentication.");
+            Console.WriteLine("\t\"SymbolsAuth.Samples.exe 2 <clientid>\" // Use default values to test symbol server authentication.");
+            Console.WriteLine("\t\"SymbolsAuth.Samples.exe 2 <clientid> C:\\SymCache Test.dll\" // Specify values to test symbol server authentication.");
         }
     }
 }
