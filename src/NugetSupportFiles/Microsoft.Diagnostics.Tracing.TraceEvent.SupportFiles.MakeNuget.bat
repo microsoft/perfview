5cd09a86a379e74d24f0cd43c99dd125869b9d6e
Brian Robbins <brianrob@microsoft.com>
2024-07-22 19:48:29 -0700

    Remove NuGetSupportFiles directory.

--- src/NugetSupportFiles/Microsoft.Diagnostics.Tracing.TraceEvent.SupportFiles.MakeNuget.bat
+++
@@ -1,6 +0,0 @@
-@if NOT EXIST "Microsoft.Diagnostics.Tracing.TraceEvent.SupportFiles\lib\native\x86\msdia140.dll" (
-   echo Error the package does not seem to be populated with binaries.  
-   echo Run Microsoft.Diagnostics.Tracing.TraceEvent.SupportFiles.Populate and update before running. 
-   exit /b 1
-)
-nuget pack Microsoft.Diagnostics.Tracing.TraceEvent.SupportFiles\Microsoft.Diagnostics.Tracing.TraceEvent.SupportFiles.nuspec
