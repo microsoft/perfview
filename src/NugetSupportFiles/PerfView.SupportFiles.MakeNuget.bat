51ec1dffe9055ab58ba1b13d1b716b36760ed895
Brian Robbins <brianrob@microsoft.com>
2024-07-22 23:59:51 -0700

    Remove NuGet SupportFiles (#2080)
    * Remove documentation for SupportFiles NuGet packages.
    
    * Remove NuGetSupportFiles directory.

--- src/NugetSupportFiles/PerfView.SupportFiles.MakeNuget.bat
+++
@@ -1,6 +0,0 @@
-@if NOT EXIST "PerfView.SupportFiles\lib\native\x86\sd.exe" (
-   echo Error the package does not seem to be populated with binaries.  
-   echo Run PerfView.SupportFiles.Populate and update before running. 
-   exit /b 1
-)
-nuget pack PerfView.SupportFiles\PerfView.SupportFiles.nuspec
