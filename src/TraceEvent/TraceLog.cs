0732e763490f296c70cb38e6857a65c21ac95d19
Mitchell Hwang <16830051+mdh1418@users.noreply.github.com>
2024-09-18 14:08:59 -0400

    [TraceEvent] Restrict extension truncating to known exts (#2111)

--- src/TraceEvent/TraceLog.cs
+++ src/TraceEvent/TraceLog.cs
@@ -3671,6 +3671,7 @@ namespace Microsoft.Diagnostics.Tracing.Etlx
         }
 
         private static char[] s_directorySeparators = { '\\', '/' };
+        private static HashSet<string> s_validExtensions = new HashSet<string>(StringComparer.OrdinalIgnoreCase) { ".dll", ".exe" };
 
         // Path  GetFileNameWithoutExtension will throw on illegal chars, which is too strong, so avoid that here.
         internal static string GetFileNameWithoutExtensionNoIllegalChars(string filePath)
@@ -3686,12 +3687,16 @@ namespace Microsoft.Diagnostics.Tracing.Etlx
             }
 
             int dotIdx = filePath.LastIndexOf('.');
-            if (dotIdx < lastDirectorySep)
+            if (dotIdx > lastDirectorySep)
             {
-                dotIdx = filePath.Length;
+                string extension = filePath.Substring(dotIdx);
+                if (s_validExtensions.Contains(extension))
+                {
+                    return filePath.Substring(lastDirectorySep, dotIdx - lastDirectorySep);
+                }
             }
 
-            return filePath.Substring(lastDirectorySep, dotIdx - lastDirectorySep);
+            return filePath.Substring(lastDirectorySep);
         }
 
 #if DEBUG
