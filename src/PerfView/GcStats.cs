eef529f2864ca15df05b405ec2de1d6300ca7d56
Mukund Raghav Sharma (Moko) <68247673+mrsharm@users.noreply.github.com>
2025-01-13 14:27:00 -0800

    Revert "Added missing time info fields in the Raw XML View in GCStats (#2140)"
    This reverts commit 8bd9ae2a9d4a233bd2b604a0f88c4b6ea60f6a35.

--- src/PerfView/GcStats.cs
+++ src/PerfView/GcStats.cs
@@ -515,7 +515,6 @@ namespace Stats
                 {
                     if (gc.TimingInfo[(int)TraceGC.TimingType.MarkRoot].HasValue) { writer.Write(" MarkRoot=\"{0}\"", gc.TimingInfo[(int)TraceGC.TimingType.MarkRoot].Value); }
                     if (gc.TimingInfo[(int)TraceGC.TimingType.MarkScanFinalization].HasValue) { writer.Write(" MarkScanFinalization=\"{0}\"", gc.TimingInfo[(int)TraceGC.TimingType.MarkScanFinalization].Value); }
-                    if (gc.TimingInfo[(int)TraceGC.TimingType.MarkShortWeak].HasValue) { writer.Write(" MarkShortWeak=\"{0}\"", gc.TimingInfo[(int)TraceGC.TimingType.MarkShortWeak].Value); }
                     if (gc.TimingInfo[(int)TraceGC.TimingType.MarkLongWeak].HasValue) { writer.Write(" MarkLongWeak=\"{0}\"", gc.TimingInfo[(int)TraceGC.TimingType.MarkLongWeak].Value); }
                     if (gc.TimingInfo[(int)TraceGC.TimingType.Plan].HasValue) { writer.Write(" Plan=\"{0}\"", gc.TimingInfo[(int)TraceGC.TimingType.Plan].Value); }
                     if (gc.TimingInfo[(int)TraceGC.TimingType.Relocate].HasValue) { writer.Write(" Relocate=\"{0}\"", gc.TimingInfo[(int)TraceGC.TimingType.Relocate].Value); }
@@ -543,49 +542,42 @@ namespace Stats
 
                         if (mt != null)
                         {
-                            void WriteDetailsAboutMarkRootType(MarkRootType type)
+                            writer.Write(" MarkStack =\"{0:n3}", mt.MarkTimes[(int)MarkRootType.MarkStack]);
+                            if (mt.MarkPromoted != null)
                             {
-                                writer.Write(" {0}=\"{1:n3}", type, mt.MarkTimes[(int)type]);
-                                if (mt.MarkPromoted != null)
-                                {
-                                    writer.Write("({0})", mt.MarkPromoted[(int)type]);
-                                }
-                                writer.Write("\"");
+                                writer.Write("({0})", mt.MarkPromoted[(int)MarkRootType.MarkStack]);
                             }
 
-                            WriteDetailsAboutMarkRootType(MarkRootType.MarkStack);
-                            WriteDetailsAboutMarkRootType(MarkRootType.MarkFQ);
-                            WriteDetailsAboutMarkRootType(MarkRootType.MarkHandles);
-
-                            // Condition:  if ((condemned_gen_number == max_generation) && (num_sizedrefs > 0))
-                            if (gc.Generation == 2)
+                            writer.Write("\" MarkFQ =\"{0:n3}", mt.MarkTimes[(int)MarkRootType.MarkFQ]);
+                            if (mt.MarkPromoted != null)
                             {
-                                WriteDetailsAboutMarkRootType(MarkRootType.MarkSizedRef);
+                                writer.Write("({0})", mt.MarkPromoted[(int)MarkRootType.MarkFQ]);
                             }
 
-                            WriteDetailsAboutMarkRootType(MarkRootType.MarkOverflow);
-                            WriteDetailsAboutMarkRootType(MarkRootType.MarkDependentHandles);
-                            WriteDetailsAboutMarkRootType(MarkRootType.MarkNewFQ);
-
-                            // Condition: if (do_mark_steal_p)
-                            // if (full_p && total_heap_size > (100 * 1024 * 1024))
-                            if (gc.Generation == 2)
+                            writer.Write("\" MarkHandles =\"{0:n3}", mt.MarkTimes[(int)MarkRootType.MarkHandles]);
+                            if (mt.MarkPromoted != null)
                             {
-                                WriteDetailsAboutMarkRootType(MarkRootType.MarkSteal);
+                                writer.Write("({0})", mt.MarkPromoted[(int)MarkRootType.MarkHandles]);
                             }
 
-                            // Condition: if (gc_heap::background_running_p())
-                            if (gc.Type == GCType.BackgroundGC || gc.Type == GCType.ForegroundGC)
-                            {
-                                WriteDetailsAboutMarkRootType(MarkRootType.MarkBGCRoots);
-                            }
-
-                            // Condition: if !Gen2
+                            writer.Write("\"");
                             if (gc.Generation != 2)
                             {
+                                writer.Write(" MarkOldGen =\"{0:n3}", mt.MarkTimes[(int)MarkRootType.MarkOlder]);
+                                if (mt.MarkPromoted != null)
+                                {
+                                    writer.Write("({0})", mt.MarkPromoted[(int)MarkRootType.MarkOlder]);
+                                }
+
                                 writer.Write("\"");
-                                WriteDetailsAboutMarkRootType(MarkRootType.MarkOlder);
-                                writer.Write("\"");
+                            }
+                            if (mt.MarkTimes[(int)MarkRootType.MarkOverflow] != 0.0)
+                            {
+                                writer.Write(" MarkOverflow =\"{0:n3}", mt.MarkTimes[(int)MarkRootType.MarkOverflow]);
+                                if (mt.MarkPromoted != null)
+                                {
+                                    writer.Write("({0})", mt.MarkPromoted[(int)MarkRootType.MarkOverflow]);
+                                }
                             }
                         }
                         if (gc.LOHCompactInfos.Count > 0)
